7236579ad10b556c90f1679af3fb26262a70056c
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-07 03:19:30 -0600

    Add mapper for league division values

---
+++ Soraka/Core/LeagueDivisionMapper.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Immutable;
+
+namespace Soraka.Core
+{
+	internal class LeagueDivisionMapper
+	{
+		private static readonly ImmutableDictionary<Type.LeagueDivision, string> _valueByLeagueDivision =
+			new Dictionary<Type.LeagueDivision, string>
+			{
+				{ Type.LeagueDivision.I, "I" },
+				{ Type.LeagueDivision.II, "II" },
+				{ Type.LeagueDivision.III, "III" },
+				{ Type.LeagueDivision.IV, "IV" }
+			}.ToImmutableDictionary();
+
+		public static string GetValue(Type.LeagueDivision leagueDivision)
+		{
+			var value = _valueByLeagueDivision.GetValueOrDefault(leagueDivision);
+			if (string.IsNullOrEmpty(value))
+				throw new NotImplementedException($"Value for league division {leagueDivision} is not implemented");
+			return value;
+		}
+	}
+}
