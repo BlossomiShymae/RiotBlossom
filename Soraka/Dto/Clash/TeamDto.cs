f4ec23cc279287fe720e25d65263f1cf4a045c9e
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-07 20:38:10 -0600

    Add TeamDto

---
+++ Soraka/Dto/Clash/TeamDto.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Immutable;
+
+namespace Soraka.Dto.Clash
+{
+	internal record TeamDto
+	{
+		public string Id { get; init; } = default!;
+		public int Tournament { get; init; }
+		public string Name { get; init; } = default!;
+		public int IconId { get; init; }
+		public int Tier { get; init; }
+		public string Captain { get; init; } = default!;
+		public string Abbreviation { get; init; } = default!;
+		public ImmutableList<PlayerDto> Players { get; init; } = ImmutableList<PlayerDto>.Empty;
+	}
+}
