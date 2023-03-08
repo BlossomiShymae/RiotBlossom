506fdeb2b60b755a2f357e1845f2e669a11909b4
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-07 20:43:37 -0600

    Add MiniSeriesDto

---
+++ Soraka/Dto/League/MiniSeriesDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Soraka.Dto.League
+{
+	internal record MiniSeriesDto
+	{
+		public int Losses { get; init; }
+		public string Progress { get; init; } = default!;
+		public int Target { get; init; }
+		public int Wins { get; init; }
+	}
+}
