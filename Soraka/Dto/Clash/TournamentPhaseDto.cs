ab23ec93742a8e2d930fcd4c96dfe5bf5a09b66b
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-07 20:40:28 -0600

    Add TournamentPhaseDto

---
+++ Soraka/Dto/Clash/TournamentPhaseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Soraka.Dto.Clash
+{
+	internal record TournamentPhaseDto
+	{
+		public int Id { get; init; }
+		public long RegistrationTime { get; init; }
+		public long StartTime { get; init; }
+		public bool Cancelled { get; init; }
+	}
+}
