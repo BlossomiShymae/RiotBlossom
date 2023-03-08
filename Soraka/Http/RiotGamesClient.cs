041b2e6633e5672d1d12b2ac5f90dc671a3dd9da
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-08 13:37:17 -0600

    Add http client for Riot Games

---
+++ Soraka/Http/RiotGamesClient.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http.Json;
+
+namespace Soraka.Http
+{
+	internal static class RiotGamesClient
+	{
+		public static Func<string, Func<string, Func<string, Task<T>>>>
+			GetFromJsonAsync<T>(HttpClient client) =>
+			(routingValue) =>
+			(uri) =>
+			async (query) =>
+			{
+				var res = await client.GetAsync($"https://{routingValue}.api.riotgames.com{uri}/{query}");
+				T? data = default;
+				if (res.IsSuccessStatusCode && (int)res.StatusCode != 204)
+					data = await res.Content.ReadFromJsonAsync<T>();
+				else
+					throw new HttpRequestException(res.StatusCode.ToString());
+				if (data == null)
+					throw new NullReferenceException(nameof(data));
+
+				return data;
+			};
+	}
+}
