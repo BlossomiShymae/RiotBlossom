4ecbc944d14b86d4d5079c340c2818c9ca112a43
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-07 22:25:38 -0600

    Add CurrentGameParticipant

---
+++ Soraka/Dto/Spectator/CurrentGameParticipant.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Immutable;
+
+namespace Soraka.Dto.Spectator
+{
+	internal class CurrentGameParticipant
+	{
+		public long ChampionId { get; init; }
+		public Perks Perks { get; init; } = new();
+		public long ProfileIconId { get; init; }
+		public bool Bot { get; init; }
+		public long TeamId { get; init; }
+		public string SummonerName { get; init; } = default!;
+		public string SummonerId { get; init; } = default!;
+		public long Spell1Id { get; init; }
+		public long Spell2Id { get; init; }
+		public ImmutableList<GameCustomizationObject> GameCustomizationObjects { get; init; } = ImmutableList<GameCustomizationObject>.Empty;
+	}
+}
