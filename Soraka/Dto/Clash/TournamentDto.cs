786b5c21970f05e1d3a3b6df0b677bf0388939be
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-07 20:41:25 -0600

    Add TournamentDto

---
+++ Soraka/Dto/Clash/TournamentDto.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Immutable;
+
+namespace Soraka.Dto.Clash
+{
+	internal record TournamentDto
+	{
+		public int Id { get; init; }
+		public int ThemeId { get; init; }
+		public string NameKey { get; init; } = default!;
+		public string NameKeySecondary { get; init; } = default!;
+		public ImmutableList<TournamentPhaseDto> Schedule { get; init; } = ImmutableList<TournamentPhaseDto>.Empty;
+	}
+}
