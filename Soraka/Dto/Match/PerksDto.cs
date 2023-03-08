1d0b575c609780ed388f38d34eacafcc8b50888b
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-07 21:48:22 -0600

    Add PerksDto

---
+++ Soraka/Dto/Match/PerksDto.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Immutable;
+
+namespace Soraka.Dto.Match
+{
+	internal record PerksDto
+	{
+		public PerkStatsDto StatPerks { get; init; } = new();
+		public ImmutableList<PerkStyleDto> Styles { get; init; } = ImmutableList<PerkStyleDto>.Empty;
+	}
+}
