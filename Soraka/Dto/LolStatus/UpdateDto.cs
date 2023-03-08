2dcfaa4fc39edb71f975cb5d226bfaa681810b9f
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-07 21:33:41 -0600

    Add UpdateDto

---
+++ Soraka/Dto/LolStatus/UpdateDto.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Immutable;
+
+namespace Soraka.Dto.LolStatus
+{
+	internal record UpdateDto
+	{
+		public int Id { get; init; }
+		public string Author { get; init; } = default!;
+		public bool Publish { get; init; }
+		public ImmutableList<string> PublishLocations { get; init; } = ImmutableList<string>.Empty;
+		public string CreatedAt { get; init; } = default!;
+		public string UpdatedAt { get; init; } = default!;
+	}
+}
