357785ca7f1d2856b801b1f16ebdfb00efad477c
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-07 21:45:09 -0600

    Add TeamDto

---
+++ Soraka/Dto/Match/TeamDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Immutable;
+
+namespace Soraka.Dto.Match
+{
+	internal record TeamDto
+	{
+		public ImmutableList<BanDto> Bans { get; init; } = ImmutableList<BanDto>.Empty;
+		public ObjectivesDto Objectives { get; init; } = new();
+		public int TeamId { get; init; }
+		public bool Win { get; init; }
+	}
+}
