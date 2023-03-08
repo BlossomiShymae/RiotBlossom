1b73570c07882f6c85976cd01dfb1e84b5418848
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-07 21:27:16 -0600

    Add ApexPlayerInfoDto

---
+++ Soraka/Dto/LolChallenges/ApexPlayerInfoDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Soraka.Dto.LolChallenges
+{
+	internal record ApexPlayerInfoDto
+	{
+		public string Puuid { get; init; } = default!;
+		public double Value { get; init; }
+		public int Position { get; init; }
+	}
+}
