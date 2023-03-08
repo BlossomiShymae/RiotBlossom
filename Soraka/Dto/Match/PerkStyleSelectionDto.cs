71d80ee6d6f6c4d6b9106af9e323f6bf563c0066
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-07 21:45:58 -0600

    Add PerkStyleSelectionDto

---
+++ Soraka/Dto/Match/PerkStyleSelectionDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Soraka.Dto.Match
+{
+	internal record PerkStyleSelectionDto
+	{
+		public int Perk { get; init; }
+		public int Var1 { get; init; }
+		public int Var2 { get; init; }
+		public int Var3 { get; init; }
+	}
+}
