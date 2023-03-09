4a5d6dcb7a5593eca34045c8c9590f4fc087d76d
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-09 05:53:26 -0600

    Add uris for LolChallenges

---
+++ Soraka/Api/LolChallengesApi.cs
@@ -0,0 +1,13 @@
+﻿namespace Soraka.Api
+{
+	internal class LolChallengesApi
+	{
+		private static readonly string _uri = "/lol/challenges/v1/challenges";
+		private static readonly string _challengesConfigUri = "/config";
+		private static readonly string _challengesPercentilesUri = "/percentiles";
+		private static readonly string _challengesConfigForChallengeIdUri = "/{0}/config";
+		private static readonly string _challengesTopPlayersByChallengeIdAndLevelUri = "/{0}/leaderboards/by-level/{1}";
+		private static readonly string _challengesPercentilesForChallengeIdUri = "/{0}/percentiles";
+		private static readonly string _challengesProgressedUri = "/lol/challenges/v1/player-data/{0}";
+	}
+}
