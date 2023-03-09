869a9f91582dc089be70f3822a74a3b161fed2a7
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-09 04:43:05 -0600

    Add base unit test

---
+++ SorakaTests/Api/SummonerApiTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace Soraka.Api.Tests
+{
+	[TestClass()]
+	public class SummonerApiTests
+	{
+		[TestMethod()]
+		public async Task UseApiTest()
+		{
+			var api = SummonerApi.UseApi(new HttpClient());
+
+			var response = await api.GetSummonerBySummonerNameAsync("uwuie%20time");
+			Trace.WriteLine(await response.Content.ReadAsStringAsync());
+
+			Assert.IsTrue(response.IsSuccessStatusCode);
+		}
+	}
+}
