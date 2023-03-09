2b45e86bbf7110f028c7f3334239862995bd12c7
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-09 05:03:30 -0600

    Add uris for Account

---
+++ Soraka/Api/AccountApi.cs
@@ -0,0 +1,11 @@
+﻿namespace Soraka.Api
+{
+	public static class AccountApi
+	{
+		private static readonly string _uri = "/riot/account/v1/accounts";
+		private static readonly string _accountByPuuidUri = "/by-puuid/{0}";
+		private static readonly string _accountByRiotIdUri = "/by-riot-id/{0}/{1}";
+		private static readonly string _accountByAccessTokenUri = "/me";
+		private static readonly string _activeShardUri = "/riot/account/v1/active-shards/by-game/{0}/by-puuid/{1}";
+	}
+}
