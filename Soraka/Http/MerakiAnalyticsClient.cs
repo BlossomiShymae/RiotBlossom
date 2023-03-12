28f103dc14acf08c6459dbaec01dc71aba4af0e9
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-11 19:41:48 -0600

    Add client for MerakiAnalytics

---
+++ Soraka/Http/MerakiAnalyticsClient.cs
@@ -0,0 +1,11 @@
+﻿namespace Soraka.Http
+{
+	public static class MerakiAnalyticsClient
+	{
+		public static GetAsyncFunc
+			GetAsync(HttpClient client) =>
+			async (uri) => await client.GetAsync($"https://cdn.merakianalytics.com/riot/lol/resources/latest/en-US{uri}");
+
+		public delegate Task<HttpResponseMessage> GetAsyncFunc(string uri);
+	}
+}
