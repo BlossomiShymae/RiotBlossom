1a038caf0544288f5d1445cc007abbb841a00296
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-11 19:36:47 -0600

    Add client for DataDragon

---
+++ Soraka/Http/DataDragonClient.cs
@@ -0,0 +1,11 @@
+﻿namespace Soraka.Http
+{
+	public static class DataDragonClient
+	{
+		public static GetAsyncFunc
+			GetAsync(HttpClient client) =>
+			async (uri) => await client.GetAsync($"https://ddragon.leagueoflegends.com{uri}");
+
+		public delegate Task<HttpResponseMessage> GetAsyncFunc(string uri);
+	}
+}
