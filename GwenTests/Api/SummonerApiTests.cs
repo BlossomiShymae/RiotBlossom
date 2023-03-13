98d4d7fa3cc3c6825401f745c3b42abfe0cc0831
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-13 11:23:40 -0500

    Add stub class for client

--- GwenTests/Api/SummonerApiTests.cs
+++ GwenTests/Api/SummonerApiTests.cs
@@ -1,26 +1,20 @@
-﻿using Gwen.Core;
-using Gwen.Dto.Summoner;
+﻿using Gwen.Dto.Summoner;
+using GwenTests;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Gwen.Api.Tests
 {
-    [TestClass()]
-    public class SummonerApiTests
-    {
-        private static readonly string _riotApiKey = Environment.GetEnvironmentVariable("RIOT_API_KEY") ?? string.Empty;
+	[TestClass()]
+	public class SummonerApiTests
+	{
+		[TestMethod()]
+		public async Task UseApiTest()
+		{
+			var gwen = StubClient.UseWidespread();
 
-        [TestMethod()]
-        public async Task UseApiTest()
-        {
-            var gwen = GwenCore.UseWidespreadClient(new GwenCore.Settings
-            {
-                HttpClient = new HttpClient(),
-                RiotApiKey = _riotApiKey,
-            });
+			var summonerDto = await gwen.Riot[Type.PlatformRoute.NorthAmerica].Summoner.GetSummonerByNameAsync("uwuie time");
 
-            var summonerDto = await gwen.Riot[Type.PlatformRoute.NorthAmerica].Summoner.GetSummonerByNameAsync("uwuie time");
-
-            Assert.IsTrue(summonerDto is SummonerDto);
-        }
-    }
+			Assert.IsTrue(summonerDto is SummonerDto);
+		}
+	}
 }
---
+++ GwenTests/StubClient.cs
@@ -0,0 +1,17 @@
+﻿using Gwen.Core;
+
+namespace GwenTests
+{
+	public static class StubClient
+	{
+		private static readonly string _riotApiKey = Environment.GetEnvironmentVariable("RIOT_API_KEY") ?? string.Empty;
+		private static readonly GwenCore.WidespreadClient _widespread = GwenCore.UseWidespreadClient(
+			new GwenCore.Settings
+			{
+				HttpClient = new HttpClient(),
+				RiotApiKey = _riotApiKey
+			});
+
+		public static GwenCore.WidespreadClient UseWidespread() => _widespread;
+	}
+}
