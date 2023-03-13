224a10116741c99c91acf8f895c9e749c6a60224
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-13 12:26:40 -0500

    Refactor tests for SummonerApi

--- GwenTests/Api/SummonerApiTests.cs
+++ GwenTests/Api/SummonerApiTests.cs
@@ -7,14 +7,56 @@ namespace Gwen.Api.Tests
 	[TestClass()]
 	public class SummonerApiTests
 	{
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+		public static SummonerDto summoner;
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+		[ClassInitialize()]
+		public static async Task Initialize(TestContext testContext)
+		{
+			var gwen = StubClient.UseWidespread();
+
+			summoner = await gwen.Riot[Type.PlatformRoute.NorthAmerica].Summoner.GetSummonerByNameAsync("uwuie time");
+		}
+
 		[TestMethod()]
-		public async Task UseApiTest()
+		public async Task Api_WithAccountId_ShouldReturnSummonerDto()
+		{
+			var gwen = StubClient.UseWidespread();
+
+			var summonerDto = await gwen.Riot[Type.PlatformRoute.NorthAmerica].Summoner.GetSummonerByAccountIdAsync(summoner.AccountId);
+
+			Assert.IsInstanceOfType(summonerDto, typeof(SummonerDto));
+		}
+
+		[TestMethod()]
+		public async Task Api_WithSummonerName_ShouldReturnSummonerDto()
 		{
 			var gwen = StubClient.UseWidespread();
 
 			var summonerDto = await gwen.Riot[Type.PlatformRoute.NorthAmerica].Summoner.GetSummonerByNameAsync("uwuie time");
 
-			Assert.IsTrue(summonerDto is SummonerDto);
+			Assert.IsInstanceOfType(summonerDto, typeof(SummonerDto));
+		}
+
+		[TestMethod()]
+		public async Task Api_WithSummonerPuuid_ShouldReturnSummonerDto()
+		{
+			var gwen = StubClient.UseWidespread();
+
+			var summonerDto = await gwen.Riot[Type.PlatformRoute.NorthAmerica].Summoner.GetSummonerByPuuidAsync(summoner.Puuid);
+
+			Assert.IsInstanceOfType(summonerDto, typeof(SummonerDto));
+		}
+
+		[TestMethod()]
+		public async Task Api_WithSummonerId_ShouldReturnSummonerDto()
+		{
+			var gwen = StubClient.UseWidespread();
+
+			var summonerDto = await gwen.Riot[Type.PlatformRoute.NorthAmerica].Summoner.GetSummonerByIdAsync(summoner.Id);
+
+			Assert.IsInstanceOfType(summonerDto, typeof(SummonerDto));
 		}
 	}
 }
