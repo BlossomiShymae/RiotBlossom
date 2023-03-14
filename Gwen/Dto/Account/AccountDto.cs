de5dca9bdec4368b425fcb551f8daba84511c1fb
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-14 17:36:54 -0500

    Add comments for AccountDto

--- Gwen/Dto/Account/AccountDto.cs
+++ Gwen/Dto/Account/AccountDto.cs
@@ -1,9 +1,18 @@
 ﻿namespace Gwen.Dto.Account
 {
-    internal record AccountDto
+    public record AccountDto
     {
+        /// <summary>
+        /// The player UUID associated with account.
+        /// </summary>
         public string Puuid { get; init; } = default!;
+        /// <summary>
+        /// The Riot game name associated with account. May be excluded from response if account does not have it..
+        /// </summary>
         public string GameName { get; init; } = default!;
+        /// <summary>
+        /// The Riot tag line associated with account. May be excluded from response if account does not have it.
+        /// </summary>
         public string TagLine { get; init; } = default!;
     }
 }
