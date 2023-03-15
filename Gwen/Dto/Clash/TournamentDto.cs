3f22aa4992cafc280937ac5c21e96f9f577550a1
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-15 15:19:25 -0500

    Add comments to TournamentDto

--- Gwen/Dto/Clash/TournamentDto.cs
+++ Gwen/Dto/Clash/TournamentDto.cs
@@ -2,12 +2,27 @@
 
 namespace Gwen.Dto.Clash
 {
-    internal record TournamentDto
+    public record TournamentDto
     {
+        /// <summary>
+        /// The tournament ID.
+        /// </summary>
         public int Id { get; init; }
+        /// <summary>
+        /// The theme ID.
+        /// </summary>
         public int ThemeId { get; init; }
+        /// <summary>
+        /// The name of tournament e.g. "bilgewater".
+        /// </summary>
         public string NameKey { get; init; } = default!;
+        /// <summary>
+        /// The secondary name of tournament e.g. "day_2".
+        /// </summary>
         public string NameKeySecondary { get; init; } = default!;
+        /// <summary>
+        /// The spanning date the tournament will take place on.
+        /// </summary>
         public ImmutableList<TournamentPhaseDto> Schedule { get; init; } = ImmutableList<TournamentPhaseDto>.Empty;
     }
 }
