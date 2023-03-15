1e955b1f286552bcf765a4a6244fdfc760eb1c9c
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-15 16:36:17 -0500

    Add comments for MiniSeriesDto

--- Gwen/Dto/League/MiniSeriesDto.cs
+++ Gwen/Dto/League/MiniSeriesDto.cs
@@ -1,10 +1,37 @@
 ﻿namespace Gwen.Dto.League
 {
-    internal record MiniSeriesDto
+    public record MiniSeriesDto
     {
+        /// <summary>
+        /// The game losses for miniseries.
+        /// </summary>
         public int Losses { get; init; }
+        /// <summary>
+        /// The string representation of current miniseries.
+        /// <list type="bullet">
+        /// <item>
+        /// <term>L</term>
+        /// <description>Loss</description>
+        /// </item>
+        /// <item>
+        /// <term>W</term>
+        /// <description>Win</description>
+        /// </item>
+        /// <item>
+        /// <term>N</term>
+        /// <description>Open/not played</description>
+        /// </item>
+        /// </list>
+        /// <example>'WLWLN' is an example progress.</example>
+        /// </summary>
         public string Progress { get; init; } = default!;
+        /// <summary>
+        /// The wins required for miniseries to be accomplished.
+        /// </summary>
         public int Target { get; init; }
+        /// <summary>
+        /// The game wins for miniseries.
+        /// </summary>
         public int Wins { get; init; }
     }
 }
