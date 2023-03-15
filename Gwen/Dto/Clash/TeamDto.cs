3dce4036f9c9d09eb388de50e6ec53c110504262
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-15 15:16:06 -0500

    Add comments for TeamDto

--- Gwen/Dto/Clash/TeamDto.cs
+++ Gwen/Dto/Clash/TeamDto.cs
@@ -2,15 +2,39 @@
 
 namespace Gwen.Dto.Clash
 {
-    internal record TeamDto
+    public record TeamDto
     {
+        /// <summary>
+        /// The team ID.
+        /// </summary>
         public string Id { get; init; } = default!;
-        public int Tournament { get; init; }
+        /// <summary>
+        /// The associated tournament ID the team is participating in.
+        /// </summary>
+        public int TournamentId { get; init; }
+        /// <summary>
+        /// The team name.
+        /// </summary>
         public string Name { get; init; } = default!;
+        /// <summary>
+        /// The icon ID of team.
+        /// </summary>
         public int IconId { get; init; }
+        /// <summary>
+        /// The rank tier of team.
+        /// </summary>
         public int Tier { get; init; }
+        /// <summary>
+        /// The encrypted summoner ID of team captain.
+        /// </summary>
         public string Captain { get; init; } = default!;
+        /// <summary>
+        /// The abbreviation of team name.
+        /// </summary>
         public string Abbreviation { get; init; } = default!;
+        /// <summary>
+        /// The current team roster.
+        /// </summary>
         public ImmutableList<PlayerDto> Players { get; init; } = ImmutableList<PlayerDto>.Empty;
     }
 }
