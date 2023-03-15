93638a291029b6ad41a008d0a3e6a5530caf1948
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-15 11:25:48 -0500

    Refactor ChampionMasteryApi to OOP paradigm

--- Gwen/Api/ChampionMasteryApi.cs
+++ Gwen/Api/ChampionMasteryApi.cs
@@ -3,55 +3,58 @@ using Gwen.Http;
 
 namespace Gwen.Api
 {
-	public static class ChampionMasteryApi
+	internal class ChampionMasteryApi
 	{
 		private static readonly string _uri = "/lol/champion-mastery/v4/champion-masteries";
 		private static readonly string _masteriesBySummonerId = _uri + "/by-summoner/{0}";
 		private static readonly string _masteryBySummonerIdAndChampionId = _masteriesBySummonerId + "/by-champion/{1}";
 		private static readonly string _masteriesTopBySummonerId = _masteriesBySummonerId + "/top";
 		private static readonly string _scoresBySummonerId = "/lol/champion-mastery/v4/scores/by-summoner/{0}";
+		private readonly ComposableApi<IEnumerable<ChampionMasteryDto>> _championMasteriesDtoApi;
+		private readonly ComposableApi<ChampionMasteryDto> _championMasteryDtoApi;
+		private readonly ComposableApi<int> _intApi;
 
-		public static ComposableApi.UseByRoutingValue<Container>
-			Use(HttpClient client, string riotApiKey, XMiddlewares middlewares) =>
-			(routingValue) =>
-			{
-				RiotGamesClient.GetAsyncFunc func = RiotGamesClient.GetAsync(client, riotApiKey, routingValue, middlewares);
-				return new Container
-				{
-					GetChampionMasteriesBySummonerIdAsync = (string summonerId) =>
-						ComposableApi.GetDtoAsync<IEnumerable<ChampionMasteryDto>>(func)(string.Format(_masteriesBySummonerId, summonerId), string.Empty),
-					GetChampionMasteryByPlayerIdAndChampionIdAsync = (string summonerId, string championId) =>
-						ComposableApi.GetDtoAsync<ChampionMasteryDto>(func)(string.Format(_masteryBySummonerIdAndChampionId, summonerId, championId), string.Empty),
-					GetTopChampionMasteriesBySummonerIdAsync = (string summonerId, int count) =>
-						ComposableApi.GetDtoAsync<IEnumerable<ChampionMasteryDto>>(func)(string.Format(_masteriesTopBySummonerId, summonerId), $"?count={count}"),
-					GetTotalChampionMasteryScoreBySummonerIdAsync = (string summonerId) =>
-						ComposableApi.GetDtoAsync<int>(func)(string.Format(_scoresBySummonerId, summonerId), string.Empty)
-				};
-			};
-
-		public record Container
+		public ChampionMasteryApi(RiotGamesClient riotGamesClient)
 		{
-			/// <summary>
-			/// Get a <see cref="IEnumerable{T}"/> of all <see cref="ChampionMasteryDto"/> for summoner ID. Sorted by <see cref="ChampionMasteryDto.ChampionPoints"/> descending.
-			/// </summary>
-			public GetChampionMasteriesBySummonerIdAsyncFunc GetChampionMasteriesBySummonerIdAsync { get; init; } = default!;
-			/// <summary>
-			/// Get a <see cref="ChampionMasteryDto"/> by player ID and champion ID.
-			/// </summary>
-			public GetChampionMasteryByPlayerIdAndChampionIdAsyncFunc GetChampionMasteryByPlayerIdAndChampionIdAsync { get; init; } = default!;
-			/// <summary>
-			/// Get a <see cref="IEnumerable{T}"/> of requested <see cref="ChampionMasteryDto"/> sorted by <see cref="ChampionMasteryDto.ChampionPoints"/> descending.
-			/// </summary>
-			public GetTopChampionMasteriesBySummonerIdAsyncFunc GetTopChampionMasteriesBySummonerIdAsync { get; init; } = default!;
-			/// <summary>
-			/// Get the total summation of individual champion mastery levels for associated summoner ID.
-			/// </summary>
-			public GetTotalChampionMasteryScoreBySummonerIdAsyncFunc GetTotalChampionMasteryScoreBySummonerIdAsync { get; init; } = default!;
+			_championMasteriesDtoApi = new(riotGamesClient);
+			_championMasteryDtoApi = new(riotGamesClient);
+			_intApi = new(riotGamesClient);
 		}
 
-		public delegate Task<IEnumerable<ChampionMasteryDto>> GetChampionMasteriesBySummonerIdAsyncFunc(string summonerId);
-		public delegate Task<ChampionMasteryDto> GetChampionMasteryByPlayerIdAndChampionIdAsyncFunc(string summonerId, string championId);
-		public delegate Task<IEnumerable<ChampionMasteryDto>> GetTopChampionMasteriesBySummonerIdAsyncFunc(string summonerId, int count);
-		public delegate Task<int> GetTotalChampionMasteryScoreBySummonerIdAsyncFunc(string summonerId);
+		/// <summary>
+		/// Get an enumerable of all champion mastery entries for summoner ID. Sorted by <see cref="ChampionMasteryDto.ChampionPoints"/>
+		/// descending.
+		/// </summary>
+		/// <param name="summonerId"></param>
+		/// <returns></returns>
+		public async Task<IEnumerable<ChampionMasteryDto>> ListBySummonerIdAsync(string summonerId)
+			=> await _championMasteriesDtoApi.GetDtoAsync(string.Format(_masteriesBySummonerId, summonerId));
+
+		/// <summary>
+		/// Get champion mastery entry by player ID and champion ID.
+		/// </summary>
+		/// <param name="summonerId"></param>
+		/// <param name="championId"></param>
+		/// <returns></returns>
+		public async Task<ChampionMasteryDto> GetBySummonerIdAndChampionIdAsync(string summonerId, string championId)
+			=> await _championMasteryDtoApi.GetDtoAsync(string.Format(_masteryBySummonerIdAndChampionId, summonerId, championId));
+
+		/// <summary>
+		/// Get an enumerable of requested champion mastery entries sorted by <see cref="ChampionMasteryDto.ChampionPoints"/>
+		/// descending.
+		/// </summary>
+		/// <param name="summonerId"></param>
+		/// <param name="count"></param>
+		/// <returns></returns>
+		public async Task<IEnumerable<ChampionMasteryDto>> ListTopBySummonerIdAsync(string summonerId, int count = 3)
+			=> await _championMasteriesDtoApi.GetDtoAsync(string.Format(_masteriesTopBySummonerId, summonerId), $"?count=${count}");
+
+		/// <summary>
+		/// Get the total summation of individual champion mastery levels for associated summoner ID.
+		/// </summary>
+		/// <param name="summonerId"></param>
+		/// <returns></returns>
+		public async Task<int> GetTotalScoreBySummonerIdAsync(string summonerId)
+			=> await _intApi.GetDtoAsync(string.Format(_scoresBySummonerId, summonerId));
 	}
 }
