352df4a382dbb3050ee8982247ce0d7b90aa189e
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-15 12:41:43 -0500

    Add BlanketStitch

---
+++ Gwen/Core/BlanketStitch.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Immutable;
+
+namespace Gwen.Core
+{
+	internal class BlanketStitch
+	{
+		private readonly ImmutableDictionary<Type.PlatformRoute, RiotCore> _riotCoreByPlatform;
+		/// <summary>
+		/// The immutable dictionary of components used to access Riot Games APIs. Accepts a key index of <see cref="Type.PlatformRoute"/>
+		/// to get associated component.
+		/// </summary>
+		public ImmutableDictionary<Type.PlatformRoute, RiotCore> Riot { get { return _riotCoreByPlatform; } }
+
+		public BlanketStitch(ImmutableDictionary<Type.PlatformRoute, RiotCore> riotCoreByPlatform)
+		{
+			_riotCoreByPlatform = riotCoreByPlatform;
+		}
+	}
+}
