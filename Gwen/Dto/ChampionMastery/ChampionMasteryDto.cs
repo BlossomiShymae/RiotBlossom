ff00e8d9248807848b5659912a435f3c046d7e3d
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-14 19:13:32 -0500

    Add comments for ChampionMasteryDto

--- Gwen/Dto/ChampionMastery/ChampionMasteryDto.cs
+++ Gwen/Dto/ChampionMastery/ChampionMasteryDto.cs
@@ -1,15 +1,43 @@
 ﻿namespace Gwen.Dto.ChampionMastery
 {
-    internal record ChampionMasteryDto
+    public record ChampionMasteryDto
     {
+        /// <summary>
+        /// The number of points needed to achieve next mastery level. Zero when player has reached
+        /// the maximum level.
+        /// </summary>
         public string ChampionPointsUntilNextLevel { get; init; } = default!;
+        /// <summary>
+        /// If hextech chest was earned in the current patch season.
+        /// </summary>
         public bool ChestGranted { get; init; }
+        /// <summary>
+        /// The associated champion ID for mastery.
+        /// </summary>
         public long ChampionId { get; init; }
+        /// <summary>
+        /// The last time in Unix epoch milliseconds the champion was played for associated summoner.
+        /// </summary>
         public long LastPlayTime { get; init; }
+        /// <summary>
+        /// The champion level for player and champion combination.
+        /// </summary>
         public int ChampionLevel { get; init; }
+        /// <summary>
+        ///  The associated summoner ID.
+        /// </summary>
         public string SummonerId { get; init; } = default!;
+        /// <summary>
+        /// The total number of champion points for player and champion combination.
+        /// </summary>
         public int ChampionPoints { get; init; }
+        /// <summary>
+        /// The number of points earned since achieved current level.
+        /// </summary>
         public long ChampionPointsSinceLastLevel { get; init; }
+        /// <summary>
+        /// The tokens earned for champion at current level. Resets to zero when <see cref="ChampionLevel"/> advances.
+        /// </summary>
         public int TokensEarned { get; init; }
     }
 }
