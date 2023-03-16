f41d009ad67c980638f96a797b191107febe84d5
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-16 18:51:20 -0500

    Add comments to GameCustomizationObject

--- Gwen/Dto/Spectator/GameCustomizationObject.cs
+++ Gwen/Dto/Spectator/GameCustomizationObject.cs
@@ -1,8 +1,14 @@
 ﻿namespace Gwen.Dto.Spectator
 {
-    internal record GameCustomizationObject
+    public record GameCustomizationObject
     {
+        /// <summary>
+        /// The category ID for Game Customization.
+        /// </summary>
         public string Category { get; init; } = default!;
+        /// <summary>
+        /// The content for Game Customization.
+        /// </summary>
         public string Content { get; init; } = default!;
     }
 }
