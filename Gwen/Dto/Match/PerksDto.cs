e7f0063346d59197cfa99fda63ae8f4d6637d388
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-16 02:23:36 -0500

    Add comments to PerksDto

--- Gwen/Dto/Match/PerksDto.cs
+++ Gwen/Dto/Match/PerksDto.cs
@@ -2,9 +2,19 @@
 
 namespace Gwen.Dto.Match
 {
-    internal record PerksDto
-    {
-        public PerkStatsDto StatPerks { get; init; } = new();
-        public ImmutableList<PerkStyleDto> Styles { get; init; } = ImmutableList<PerkStyleDto>.Empty;
-    }
+	/// <summary>
+	/// A game data object that represents selected options in Runes Reforged.
+	/// See CommunityDragon <see href="https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/perks.json">perks.json</see>.
+	/// </summary>
+	public record PerksDto
+	{
+		/// <summary>
+		/// The selected stat perks for a player.
+		/// </summary>
+		public PerkStatsDto StatPerks { get; init; } = new();
+		/// <summary>
+		/// The selected perk styles for a player.
+		/// </summary>
+		public ImmutableList<PerkStyleDto> Styles { get; init; } = ImmutableList<PerkStyleDto>.Empty;
+	}
 }
