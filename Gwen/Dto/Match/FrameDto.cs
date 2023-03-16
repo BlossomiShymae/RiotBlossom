8fa3163c09cb1b1df2614264eed6f2f7fd9d6af7
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-16 05:17:46 -0500

    Add FrameDto

---
+++ Gwen/Dto/Match/FrameDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Immutable;
+
+namespace Gwen.Dto.Match
+{
+	public record FrameDto
+	{
+		public ImmutableList<EventDto> Events { get; init; } = ImmutableList<EventDto>.Empty;
+		public ImmutableDictionary<string, ParticipantFrameDto> ParticipantFrames { get; init; } = ImmutableDictionary<string, ParticipantFrameDto>.Empty;
+		public int Timestamp { get; init; }
+	}
+}
