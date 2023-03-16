41d6311dc584d958db8d0b37b78b599e88392c83
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-16 04:41:58 -0500

    Add comments to MatchDto

--- Gwen/Dto/Match/MatchDto.cs
+++ Gwen/Dto/Match/MatchDto.cs
@@ -1,8 +1,14 @@
 ﻿namespace Gwen.Dto.Match
 {
-    internal record MatchDto
+    public record MatchDto
     {
+        /// <summary>
+        /// The metadata of match.
+        /// </summary>
         public MetadataDto Metadata { get; init; } = new();
+        /// <summary>
+        /// The info of match. The yummy part that most developers want. :eyes:
+        /// </summary>
         public InfoDto Info { get; init; } = new();
     }
 }
