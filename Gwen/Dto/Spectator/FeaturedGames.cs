f930f6047b19fef847d4393fc2c7da2cc2e6baa6
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-16 18:50:25 -0500

    Add comments to FeaturedGames

--- Gwen/Dto/Spectator/FeaturedGames.cs
+++ Gwen/Dto/Spectator/FeaturedGames.cs
@@ -2,9 +2,15 @@
 
 namespace Gwen.Dto.Spectator
 {
-    internal record FeaturedGames
+    public record FeaturedGames
     {
+        /// <summary>
+        /// The list of featured games.
+        /// </summary>
         public ImmutableList<FeaturedGameInfo> GameList { get; init; } = ImmutableList<FeaturedGameInfo>.Empty;
+        /// <summary>
+        /// The suggested interval to wait before making another request for refreshing.
+        /// </summary>
         public long ClientRefreshInterval { get; init; }
     }
 }
