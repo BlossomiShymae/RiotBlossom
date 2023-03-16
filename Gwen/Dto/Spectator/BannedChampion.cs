ddb9216b013f230f3e2df02ddcb2b04176f74690
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-16 18:44:01 -0500

    Add comments to BannedChampion

--- Gwen/Dto/Spectator/BannedChampion.cs
+++ Gwen/Dto/Spectator/BannedChampion.cs
@@ -1,9 +1,18 @@
 ﻿namespace Gwen.Dto.Spectator
 {
-    internal record BannedChampion
+    public record BannedChampion
     {
+        /// <summary>
+        /// The Draft Pick turn for when the champion was banned.
+        /// </summary>
         public int PickTurn { get; init; }
+        /// <summary>
+        /// The champion ID.
+        /// </summary>
         public long ChampionId { get; init; }
+        /// <summary>
+        /// The banning team ID.
+        /// </summary>
         public long TeamId { get; init; }
     }
 }
