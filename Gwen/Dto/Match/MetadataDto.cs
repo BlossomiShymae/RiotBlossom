33283baa159e09404bd1249227b9309459e0008f
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-16 04:39:33 -0500

    Add comments to MetadataDto

--- Gwen/Dto/Match/MetadataDto.cs
+++ Gwen/Dto/Match/MetadataDto.cs
@@ -2,10 +2,19 @@
 
 namespace Gwen.Dto.Match
 {
-    internal record MetadataDto
+    public record MetadataDto
     {
+        /// <summary>
+        /// The data version of match.
+        /// </summary>
         public string DataVersion { get; init; } = default!;
+        /// <summary>
+        /// The match ID.
+        /// </summary>
         public string MatchId { get; init; } = default!;
+        /// <summary>
+        /// The list of encrypted PUUIDs for summoners that participated in the match.
+        /// </summary>
         public ImmutableList<string> Participants { get; init; } = ImmutableList<string>.Empty;
     }
 }
