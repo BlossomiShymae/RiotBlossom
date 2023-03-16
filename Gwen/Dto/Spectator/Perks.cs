f72f88ca3fd256e731b726e396c8b75cbf7493b4
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-16 18:57:31 -0500

    Add comments to Perks

--- Gwen/Dto/Spectator/Perks.cs
+++ Gwen/Dto/Spectator/Perks.cs
@@ -2,10 +2,19 @@
 
 namespace Gwen.Dto.Spectator
 {
-    internal record Perks
+    public record Perks
     {
+        /// <summary>
+        /// The list of equipped perk/runes IDs.
+        /// </summary>
         public ImmutableList<long> PerkIds { get; init; } = ImmutableList<long>.Empty;
+        /// <summary>
+        /// The primary runes path.
+        /// </summary>
         public long PerkStyle { get; init; }
+        /// <summary>
+        /// The secondary runes path.
+        /// </summary>
         public long PerkSubStyle { get; init; }
     }
 }
