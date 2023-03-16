a26875ff39fdcf461e5816cedd5d21181b917d61
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-16 02:19:08 -0500

    Add comments to PerkStyleDto

--- Gwen/Dto/Match/PerkStyleDto.cs
+++ Gwen/Dto/Match/PerkStyleDto.cs
@@ -2,10 +2,19 @@
 
 namespace Gwen.Dto.Match
 {
-    internal record PerkStyleDto
+    public record PerkStyleDto
     {
+        /// <summary>
+        /// The description of perk style.
+        /// </summary>
         public string Description { get; init; } = default!;
+        /// <summary>
+        /// The selected perk styles.
+        /// </summary>
         public ImmutableList<PerkStyleSelectionDto> Selections { get; init; } = ImmutableList<PerkStyleSelectionDto>.Empty;
+        /// <summary>
+        /// The style ID.
+        /// </summary>
         public int Style { get; init; }
     }
 }
