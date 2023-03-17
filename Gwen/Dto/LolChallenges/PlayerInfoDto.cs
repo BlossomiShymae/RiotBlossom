520a1efe25243dea23c9df46ae1e16f1228da01c
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-16 19:46:15 -0500

    Add PlayerInfoDto

---
+++ Gwen/Dto/LolChallenges/PlayerInfoDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Immutable;
+
+namespace Gwen.Dto.LolChallenges
+{
+	public record PlayerInfoDto
+	{
+		public ImmutableList<ChallengeInfo> Challenges { get; init; } = ImmutableList<ChallengeInfo>.Empty;
+		public PlayerClientPreferences Preferences { get; init; } = new();
+		public ChallengePoints TotalPoints { get; init; } = new();
+		public ImmutableDictionary<string, ChallengePoints> CategoryPoints { get; init; } = ImmutableDictionary<string, ChallengePoints>.Empty;
+	}
+}
