099cd57cf5811affd2cfeb521e82a2b647dad485
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-16 19:41:58 -0500

    Add PlayerClientPreferences

---
+++ Gwen/Dto/LolChallenges/PlayerClientPreferences.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Immutable;
+
+namespace Gwen.Dto.LolChallenges
+{
+	public record PlayerClientPreferences
+	{
+		public string BannerAccent { get; init; } = default!;
+		public string Title { get; init; } = default!;
+		public ImmutableList<int> ChallengeIds { get; init; } = ImmutableList<int>.Empty;
+	}
+}
