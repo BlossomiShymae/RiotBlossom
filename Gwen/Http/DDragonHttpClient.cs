8b9e6a61d5309fa6802753051fef2070ecd14da6
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-17 16:14:22 -0500

    Refactor http client for DDragon

---
+++ Gwen/Http/DDragonHttpClient.cs
@@ -0,0 +1,17 @@
+﻿namespace Gwen.Http
+{
+    internal class DDragonHttpClient : IHttpClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public DDragonHttpClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<string> GetStringAsync(string uri)
+        {
+            return await _httpClient.GetStringAsync($"https://ddragon.leagueoflegends.com{uri}");
+        }
+    }
+}
