867f40eaa6de05b234dc969ba483d8abaabedbb5
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-17 01:47:58 -0500

    Add TacticalInfo

---
+++ Gwen/Dto/CDragon/TacticalInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace Gwen.Dto.CDragon
+{
+	public record TacticalInfo
+	{
+		public int Style { get; init; }
+		public int Difficulty { get; init; }
+		public string DamageType { get; init; } = default!;
+	}
+}
