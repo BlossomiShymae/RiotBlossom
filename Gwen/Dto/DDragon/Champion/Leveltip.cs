a765309dcc0bab4d5b635cbd71d111c327563f2d
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-17 02:15:53 -0500

    Add Leveltip

---
+++ Gwen/Dto/DDragon/Champion/Leveltip.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Immutable;
+
+namespace Gwen.Dto.DDragon.Champion
+{
+	public record Leveltip
+	{
+		public ImmutableDictionary<int, string> Label { get; init; } = ImmutableDictionary<int, string>.Empty;
+		public ImmutableDictionary<int, string> Effect { get; init; } = ImmutableDictionary<int, string>.Empty;
+	}
+}
