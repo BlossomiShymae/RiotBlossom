fec9d95bb095604172f53d5d0bdffcae4775daa7
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-17 15:11:20 -0500

    Add http client for CommunityDragon

---
+++ Gwen/Http/CDragonHttpClient.cs
@@ -0,0 +1,17 @@
+﻿namespace Gwen.Http
+{
+	internal class CDragonHttpClient : IHttpClient
+	{
+		private readonly HttpClient _httpClient;
+
+		public CDragonHttpClient(HttpClient httpClient)
+		{
+			_httpClient = httpClient;
+		}
+
+		public async Task<string> GetStringAsync(string uri)
+		{
+			return await _httpClient.GetStringAsync($"https://raw.communitydragon.org{uri}");
+		}
+	}
+}
