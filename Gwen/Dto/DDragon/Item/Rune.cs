9f7ce88b3ea206a7006dfc71e33579bafbeb5816
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-17 12:10:19 -0500

    Add Rune

---
+++ Gwen/Dto/DDragon/Item/Rune.cs
@@ -0,0 +1,9 @@
+﻿namespace Gwen.Dto.DDragon.Item
+{
+	public record Rune
+	{
+		public bool IsRune { get; init; }
+		public int Tier { get; init; }
+		public string Type { get; init; } = default!;
+	}
+}
