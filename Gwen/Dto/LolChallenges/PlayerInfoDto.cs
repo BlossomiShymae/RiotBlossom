3c9743fed3235334c454bb2f604ccbf8f89caae2
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-16 20:45:08 -0500

    Add comments to PlayerInfoDto

--- Gwen/Dto/LolChallenges/PlayerInfoDto.cs
+++ Gwen/Dto/LolChallenges/PlayerInfoDto.cs
@@ -4,9 +4,21 @@ namespace Gwen.Dto.LolChallenges
 {
 	public record PlayerInfoDto
 	{
+		/// <summary>
+		/// The list of challenges for player.
+		/// </summary>
 		public ImmutableList<ChallengeInfo> Challenges { get; init; } = ImmutableList<ChallengeInfo>.Empty;
+		/// <summary>
+		/// The player client preferences.
+		/// </summary>
 		public PlayerClientPreferences Preferences { get; init; } = new();
+		/// <summary>
+		/// The challenge points information for player.
+		/// </summary>
 		public ChallengePoints TotalPoints { get; init; } = new();
+		/// <summary>
+		/// The map of challenge point information by category e.g. "TEAMWORK", "EXPERTISE", "IMAGINATION", "VETERANCY", "COLLECTION".
+		/// </summary>
 		public ImmutableDictionary<string, ChallengePoints> CategoryPoints { get; init; } = ImmutableDictionary<string, ChallengePoints>.Empty;
 	}
 }
