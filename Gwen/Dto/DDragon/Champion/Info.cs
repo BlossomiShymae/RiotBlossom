d6931742402a86639e0c9e5174b5e98e4a91c31f
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-17 02:25:44 -0500

    Add Info

---
+++ Gwen/Dto/DDragon/Champion/Info.cs
@@ -0,0 +1,10 @@
+﻿namespace Gwen.Dto.DDragon.Champion
+{
+	public record Info
+	{
+		public int Attack { get; init; }
+		public int Defense { get; init; }
+		public int Magic { get; init; }
+		public int Difficulty { get; init; }
+	}
+}
