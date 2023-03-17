42f201920b8fffa93241237312523789ee6eabc8
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-17 12:09:10 -0500

    Add Gold

---
+++ Gwen/Dto/DDragon/Item/Gold.cs
@@ -0,0 +1,10 @@
+﻿namespace Gwen.Dto.DDragon.Item
+{
+	public record Gold
+	{
+		public int Base { get; init; }
+		public int Total { get; init; }
+		public int Sell { get; init; }
+		public bool Purchasable { get; init; }
+	}
+}
