aa61a62d3190686555537ff6a2c73e287d5ace03
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-16 19:40:31 -0500

    Add ChallengeInfo

---
+++ Gwen/Dto/LolChallenges/ChallengeInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace Gwen.Dto.LolChallenges
+{
+	public record ChallengeInfo
+	{
+		public string Level { get; init; } = default!;
+		public int Current { get; init; }
+		public int Max { get; init; }
+		public int Percentile { get; init; }
+	}
+}
