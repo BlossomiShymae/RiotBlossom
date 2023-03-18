b6a4448674c24020a45f87b8b9186a34dbc0a819
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-18 06:04:51 -0500

    Add Slot

---
+++ Gwen/Dto/DDragon/Perk/Slot.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Immutable;
+
+namespace Gwen.Dto.DDragon.Perk
+{
+	public record Slot
+	{
+		public ImmutableList<PerkRune> Runes { get; init; } = ImmutableList<PerkRune>.Empty;
+	}
+}
