34ce5cd9ddf67675275ea168864a37c4ee4bb2cb
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-19 05:57:31 -0500

    Append to previous commit (Visual Studio omg :angry_kitten:)

--- Gwen/XMiddleware/XRetryer.cs
+++ Gwen/XMiddleware/XRetryer.cs
@@ -1,29 +1,29 @@
 ﻿namespace Gwen.XMiddleware
 {
-    public class XRetry : IRetryMiddleware
-    {
-        public static XRetry Default { get; } = new XRetry();
+	public class XRetry : IRetryMiddleware
+	{
+		public static XRetry Default { get; } = new XRetry();
 
-        public async Task<HttpResponseMessage> UseRetry(Func<Task<HttpResponseMessage>> resFunc)
-        {
-            var retryAfterSeconds = 15;
-            HttpResponseMessage? responseMessage = null;
-            while (true)
-            {
-                try
-                {
-                    responseMessage = await resFunc();
-                    if ((int)responseMessage.StatusCode == 429)
-                        throw new HttpRequestException(responseMessage.ReasonPhrase, null, System.Net.HttpStatusCode.TooManyRequests);
-                }
-                catch (Exception ex)
-                {
-                    throw new SystemException(ex.Message);
-                }
-                retryAfterSeconds *= 2;
-                if (responseMessage != null)
-                    return responseMessage;
-            }
-        }
-    }
+		public async Task<HttpResponseMessage> UseRetry(Func<Task<HttpResponseMessage>> resFunc)
+		{
+			var retryAfterSeconds = 15;
+			while (true)
+			{
+				HttpResponseMessage? responseMessage;
+				try
+				{
+					responseMessage = await resFunc();
+					if ((int)responseMessage.StatusCode == 429)
+						throw new HttpRequestException(responseMessage.ReasonPhrase, null, System.Net.HttpStatusCode.TooManyRequests);
+				}
+				catch (Exception ex)
+				{
+					throw new SystemException(ex.Message);
+				}
+				retryAfterSeconds *= 2;
+				if (responseMessage != null)
+					return responseMessage;
+			}
+		}
+	}
 }
