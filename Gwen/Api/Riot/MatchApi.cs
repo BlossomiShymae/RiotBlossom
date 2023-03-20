cec0ac666b5a78fff08427cbd581170d4398d9e9
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-19 23:48:15 -0500

    Refactor MatchApi to use regions and read-only collections

--- Gwen/Api/Riot/MatchApi.cs
+++ Gwen/Api/Riot/MatchApi.cs
@@ -1,36 +1,38 @@
 ﻿using Gwen.Core;
 using Gwen.Dto.Riot.Match;
 using Gwen.Http;
+using Gwen.Type;
+using System.Collections.ObjectModel;
 
 namespace Gwen.Api.Riot
 {
-    public interface IMatchApi
+	public interface IMatchApi
 	{
 		/// <summary>
 		/// Get a match by ID/
 		/// </summary>
 		/// <param name="id"></param>
 		/// <returns></returns>
-		Task<MatchDto> GetByIdAsync(string id);
+		Task<MatchDto> GetByIdAsync(RegionalRoute regionalRoute, string id);
 		/// <summary>
 		/// Get a match timeline by ID.
 		/// </summary>
 		/// <param name="id"></param>
 		/// <returns></returns>
-		Task<MatchTimelineDto> GetTimelineByIdAsync(string id);
+		Task<MatchTimelineDto> GetTimelineByIdAsync(RegionalRoute regionalRoute, string id);
 		/// <summary>
 		/// List the last 20 most recent match IDs for encrypted PUUID.
 		/// </summary>
 		/// <param name="puuid"></param>
 		/// <returns></returns>
-		Task<IEnumerable<string>> ListIdsByPuuidAsync(string puuid);
+		Task<ReadOnlyCollection<string>> ListIdsByPuuidAsync(RegionalRoute regionalRoute, string puuid);
 		/// <summary>
 		/// List the match IDs for encrypted PUUID with given option constraints.
 		/// </summary>
 		/// <param name="puuid"></param>
 		/// <param name="parameters"></param>
 		/// <returns></returns>
-		Task<IEnumerable<string>> ListIdsByPuuidAsync(string puuid, ListIdsByPuuidAsyncOptions parameters);
+		Task<ReadOnlyCollection<string>> ListIdsByPuuidAsync(RegionalRoute regionalRoute, string puuid, ListIdsByPuuidAsyncOptions parameters);
 	}
 
 	internal class MatchApi : IMatchApi
@@ -39,7 +41,7 @@ namespace Gwen.Api.Riot
 		private static readonly string _matchIdsByPuuidUri = _uri + "/by-puuid/{0}/ids";
 		private static readonly string _matchByMatchIdUri = _uri + "/{0}";
 		private static readonly string _matchTimelineByMatchIdUri = _matchByMatchIdUri + "/timeline";
-		private readonly ComposableApi<IEnumerable<string>> _stringsApi;
+		private readonly ComposableApi<List<string>> _stringsApi;
 		private readonly ComposableApi<MatchDto> _matchApi;
 		private readonly ComposableApi<MatchTimelineDto> _matchTimelineApi;
 
@@ -50,17 +52,23 @@ namespace Gwen.Api.Riot
 			_matchTimelineApi = new(riotGamesClient);
 		}
 
-		public async Task<IEnumerable<string>> ListIdsByPuuidAsync(string puuid)
-			=> await _stringsApi.GetValueAsync(string.Format(_matchIdsByPuuidUri, puuid));
+		public async Task<ReadOnlyCollection<string>> ListIdsByPuuidAsync(RegionalRoute regionalRoute, string puuid)
+		{
+			List<string> ids = await _stringsApi.GetValueAsync(RegionRouteMapper.GetRegion(regionalRoute), string.Format(_matchIdsByPuuidUri, puuid));
+			return ids.AsReadOnly();
+		}
 
-		public async Task<IEnumerable<string>> ListIdsByPuuidAsync(string puuid, ListIdsByPuuidAsyncOptions parameters)
-			=> await _stringsApi.GetValueAsync(string.Format(_matchIdsByPuuidUri, puuid) + PropertiesToQueryConverter.ToQuery(parameters));
+		public async Task<ReadOnlyCollection<string>> ListIdsByPuuidAsync(RegionalRoute regionalRoute, string puuid, ListIdsByPuuidAsyncOptions parameters)
+		{
+			List<string> ids = await _stringsApi.GetValueAsync(RegionRouteMapper.GetRegion(regionalRoute), string.Format(_matchIdsByPuuidUri, puuid) + PropertiesToQueryConverter.ToQuery(parameters));
+			return ids.AsReadOnly();
+		}
 
-		public async Task<MatchDto> GetByIdAsync(string id)
-			=> await _matchApi.GetValueAsync(string.Format(_matchByMatchIdUri, id));
+		public async Task<MatchDto> GetByIdAsync(RegionalRoute regionalRoute, string id)
+			=> await _matchApi.GetValueAsync(RegionRouteMapper.GetRegion(regionalRoute), string.Format(_matchByMatchIdUri, id));
 
-		public async Task<MatchTimelineDto> GetTimelineByIdAsync(string id)
-			=> await _matchTimelineApi.GetValueAsync(string.Format(_matchTimelineByMatchIdUri, id));
+		public async Task<MatchTimelineDto> GetTimelineByIdAsync(RegionalRoute regionalRoute, string id)
+			=> await _matchTimelineApi.GetValueAsync(RegionRouteMapper.GetRegion(regionalRoute), string.Format(_matchTimelineByMatchIdUri, id));
 	}
 
 	public record ListIdsByPuuidAsyncOptions
