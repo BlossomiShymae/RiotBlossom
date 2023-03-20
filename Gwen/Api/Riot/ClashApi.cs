3452a6213d8891631a43cb2cedd54d6e82ff4903
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-19 22:03:04 -0500

    Refactor ClashApi to use platform and read-only collections

--- Gwen/Api/Riot/ClashApi.cs
+++ Gwen/Api/Riot/ClashApi.cs
@@ -1,40 +1,42 @@
 ﻿using Gwen.Dto.Riot.Clash;
 using Gwen.Http;
+using Gwen.Type;
+using System.Collections.ObjectModel;
 
 namespace Gwen.Api.Riot
 {
-    public interface IClashApi
+	public interface IClashApi
 	{
 		/// <summary>
 		/// Get a clash team by ID.
 		/// </summary>
 		/// <param name="teamId"></param>
 		/// <returns></returns>
-		Task<TeamDto> GetTeamByIdAsync(string teamId);
+		Task<TeamDto> GetTeamByIdAsync(PlatformRoute platformRoute, string teamId);
 		/// <summary>
 		/// Get a clash tournament by ID.
 		/// </summary>
 		/// <param name="tournamentId"></param>
 		/// <returns></returns>
-		Task<TournamentDto> GetTournamentByIdAsync(string tournamentId);
+		Task<TournamentDto> GetTournamentByIdAsync(PlatformRoute platformRoute, string tournamentId);
 		/// <summary>
 		/// Get a clash tournament by team ID.
 		/// </summary>
 		/// <param name="teamId"></param>
 		/// <returns></returns>
-		Task<TournamentDto> GetTournamentByTeamIdAsync(string teamId);
+		Task<TournamentDto> GetTournamentByTeamIdAsync(PlatformRoute platformRoute, string teamId);
 		/// <summary>
 		/// List all active or upcoming clash tournaments.
 		/// </summary>
 		/// <returns></returns>
-		Task<IEnumerable<TournamentDto>> ListActiveTournamentsAsync();
+		Task<ReadOnlyCollection<TournamentDto>> ListActiveTournamentsAsync(PlatformRoute platformRoute);
 		/// <summary>
 		/// List active Clash players for encrypted summoner ID. If a summoner registers for multiple tournaments
 		/// at once, then both registrations will appear.
 		/// </summary>
 		/// <param name="summonerId"></param>
 		/// <returns></returns>
-		Task<IEnumerable<PlayerDto>> ListPlayersBySummonerIdAsync(string summonerId);
+		Task<ReadOnlyCollection<PlayerDto>> ListPlayersBySummonerIdAsync(PlatformRoute platformRoute, string summonerId);
 	}
 
 	internal class ClashApi : IClashApi
@@ -45,9 +47,9 @@ namespace Gwen.Api.Riot
 		private static readonly string _tournamentsUri = _uri + "/tournaments";
 		private static readonly string _tournamentByTeamIdUri = _tournamentsUri + "/by-team/{0}";
 		private static readonly string _tournamentById = _tournamentsUri + "/{0}";
-		private readonly ComposableApi<IEnumerable<PlayerDto>> _playerDtosApi;
+		private readonly ComposableApi<List<PlayerDto>> _playerDtosApi;
 		private readonly ComposableApi<TeamDto> _teamDtoApi;
-		private readonly ComposableApi<IEnumerable<TournamentDto>> _tournamentDtosApi;
+		private readonly ComposableApi<List<TournamentDto>> _tournamentDtosApi;
 		private readonly ComposableApi<TournamentDto> _tournamentDtoApi;
 
 		public ClashApi(RiotHttpClient riotGamesClient)
@@ -58,19 +60,25 @@ namespace Gwen.Api.Riot
 			_tournamentDtoApi = new(riotGamesClient);
 		}
 
-		public async Task<IEnumerable<PlayerDto>> ListPlayersBySummonerIdAsync(string summonerId)
-			=> await _playerDtosApi.GetValueAsync(string.Format(_playersBySummonerIdUri, summonerId));
+		public async Task<ReadOnlyCollection<PlayerDto>> ListPlayersBySummonerIdAsync(PlatformRoute platformRoute, string summonerId)
+		{
+			List<PlayerDto> dtoCollection = await _playerDtosApi.GetValueAsync(string.Format(_playersBySummonerIdUri, summonerId));
+			return dtoCollection.AsReadOnly();
+		}
 
-		public async Task<TeamDto> GetTeamByIdAsync(string teamId)
+		public async Task<TeamDto> GetTeamByIdAsync(PlatformRoute platformRoute, string teamId)
 			=> await _teamDtoApi.GetValueAsync(string.Format(_teamByTeamIdUri, teamId));
 
-		public async Task<IEnumerable<TournamentDto>> ListActiveTournamentsAsync()
-			=> await _tournamentDtosApi.GetValueAsync(_tournamentsUri);
+		public async Task<ReadOnlyCollection<TournamentDto>> ListActiveTournamentsAsync(PlatformRoute platformRoute)
+		{
+			List<TournamentDto> dtoCollection = await _tournamentDtosApi.GetValueAsync(_tournamentsUri);
+			return dtoCollection.AsReadOnly();
+		}
 
-		public async Task<TournamentDto> GetTournamentByTeamIdAsync(string teamId)
+		public async Task<TournamentDto> GetTournamentByTeamIdAsync(PlatformRoute platformRoute, string teamId)
 			=> await _tournamentDtoApi.GetValueAsync(string.Format(_tournamentByTeamIdUri, teamId));
 
-		public async Task<TournamentDto> GetTournamentByIdAsync(string tournamentId)
+		public async Task<TournamentDto> GetTournamentByIdAsync(PlatformRoute platformRoute, string tournamentId)
 			=> await _tournamentDtoApi.GetValueAsync(string.Format(_tournamentById, tournamentId));
 	}
 }
