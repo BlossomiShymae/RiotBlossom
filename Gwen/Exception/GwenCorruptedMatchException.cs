57ccbf754acaea59c082976b6dd036518e1233ff
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-20 18:06:59 -0500

    Add exception for corrupted match

---
+++ Gwen/Exception/GwenCorruptedMatchException.cs
@@ -0,0 +1,17 @@
+﻿namespace Gwen.Exception
+{
+	/// <summary>
+	/// <para>An exception class for a corrupted Riot API game match.</para>
+	/// <see href="https://github.com/RiotGames/developer-relations/issues/642"/>
+	/// </summary>
+	public class GwenCorruptedMatchException : System.Exception
+	{
+		public GwenCorruptedMatchException(string matchId) : base(matchId)
+		{
+		}
+
+		public GwenCorruptedMatchException(string matchId, System.Exception inner) : base(matchId, inner)
+		{
+		}
+	}
+}
