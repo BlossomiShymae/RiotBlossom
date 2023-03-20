b137dc9201e4b008a8fe6ea7f4ec940508260de0
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-20 00:18:24 -0500

    Add tests for SpectatorApi

---
+++ GwenTests/Api/Riot/SpectatorApiTests.cs
@@ -0,0 +1,55 @@
+﻿using Gwen.Core.Wrapper;
+using Gwen.Dto.Riot.Spectator;
+using Gwen.Dto.Riot.Summoner;
+using Gwen.Type;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GwenTests.Api.Riot
+{
+	[TestClass()]
+	public class SpectatorApiTests
+	{
+		public static SummonerDto summoner = default!;
+
+		[ClassInitialize()]
+		public static async Task Initialize(TestContext testContext)
+		{
+			ISimpleWrapper gwen = StubClient.SimpleWrapper;
+
+			summoner = await gwen.Riot.Summoner.GetByNameAsync(PlatformRoute.NorthAmerica, "uwuie time");
+		}
+
+		[TestMethod()]
+		public async Task Api_WithSummonerId_ShouldReturnCurrentGameInfo()
+		{
+			ISimpleWrapper gwen = StubClient.SimpleWrapper;
+
+			try
+			{
+				CurrentGameInfo info = await gwen.Riot.Spectator.GetCurrentGameInfoBySummonerIdAsync(PlatformRoute.NorthAmerica, summoner.Id);
+
+				Assert.IsInstanceOfType(info, typeof(CurrentGameInfo));
+			}
+			catch (InvalidOperationException ex)
+			{
+				if (!ex.Message.Contains("404"))
+					Assert.Fail();
+			}
+			catch (Exception)
+			{
+				Assert.Fail();
+			}
+
+		}
+
+		[TestMethod()]
+		public async Task Api_ByDefault_ShouldReturnFeaturedGames()
+		{
+			ISimpleWrapper gwen = StubClient.SimpleWrapper;
+
+			FeaturedGames games = await gwen.Riot.Spectator.GetFeaturedGamesAsync(PlatformRoute.NorthAmerica);
+
+			Assert.IsInstanceOfType(games, typeof(FeaturedGames));
+		}
+	}
+}
