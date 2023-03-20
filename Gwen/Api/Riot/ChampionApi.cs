2794f963c69d5df492a47e6091b82a81e08c3879
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-19 20:32:31 -0500

    Refactor ChampionApi to use platform

--- Gwen/Api/Riot/ChampionApi.cs
+++ Gwen/Api/Riot/ChampionApi.cs
@@ -1,15 +1,17 @@
-﻿using Gwen.Dto.Riot.Champion;
+﻿using Gwen.Core;
+using Gwen.Dto.Riot.Champion;
 using Gwen.Http;
+using Gwen.Type;
 
 namespace Gwen.Api.Riot
 {
-    public interface IChampionApi
+	public interface IChampionApi
 	{
 		/// <summary>
 		/// Get the current champion rotation pools.
 		/// </summary>
 		/// <returns></returns>
-		Task<IEnumerable<ChampionInfo>> ListAsync();
+		Task<IEnumerable<ChampionInfo>> ListAsync(PlatformRoute platformRoute);
 	}
 
 	internal class ChampionApi : IChampionApi
@@ -22,7 +24,7 @@ namespace Gwen.Api.Riot
 			_championInfosApi = new(riotGamesClient);
 		}
 
-		public async Task<IEnumerable<ChampionInfo>> ListAsync()
-			=> await _championInfosApi.GetValueAsync(_championRotationsUri);
+		public async Task<IEnumerable<ChampionInfo>> ListAsync(PlatformRoute platformRoute)
+			=> await _championInfosApi.GetValueAsync(PlatformRouteMapper.GetId(platformRoute), _championRotationsUri);
 	}
 }
