f9bab439f66365b460122a049baaa9dc4d8ed88e
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-20 19:29:22 -0500

    Add docs to PlatformRoute

--- Gwen/Type/PlatformRoute.cs
+++ Gwen/Type/PlatformRoute.cs
@@ -1,22 +1,29 @@
-﻿namespace Gwen.Type
+﻿using Gwen.Core;
+
+namespace Gwen.Type
 {
-    public enum PlatformRoute
-    {
-        NorthAmerica,
-        Brazil,
-        LatinAmericaSouth,
-        LatinAmericaNorth,
-        EuropeWest,
-        EuropeNordicEast,
-        Turkey,
-        Russia,
-        Korea,
-        Japan,
-        Oceania,
-        Philippines,
-        Singapore,
-        Thailand,
-        Taiwan,
-        Vietnam
-    }
+	/// <summary>
+	/// <para>An enum that serves as a representation for platform routing values.</para>
+	/// <para>See usage with <see cref="PlatformRouteMapper"/>.</para>
+	/// <see href="https://developer.riotgames.com/docs/lol#routing-values_platform-routing-values"/>
+	/// </summary>
+	public enum PlatformRoute
+	{
+		NorthAmerica,
+		Brazil,
+		LatinAmericaSouth,
+		LatinAmericaNorth,
+		EuropeWest,
+		EuropeNordicEast,
+		Turkey,
+		Russia,
+		Korea,
+		Japan,
+		Oceania,
+		Philippines,
+		Singapore,
+		Thailand,
+		Taiwan,
+		Vietnam
+	}
 }
