f5401c4d392122923e4aefdc719d5c6f6df733b4
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-20 19:22:19 -0500

    Add comments to ChallengeLevel

--- Gwen/Type/ChallengeLevel.cs
+++ Gwen/Type/ChallengeLevel.cs
@@ -1,5 +1,9 @@
 ﻿namespace Gwen.Type
 {
+	/// <summary>
+	/// <para>An enum that represents the possible Challenge levels.</para>
+	/// <see href="https://developer.riotgames.com/apis#lol-challenges-v1/GET_getChallengeLeaderboards"/>
+	/// </summary>
 	public enum ChallengeLevel
 	{
 		None,
