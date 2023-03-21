cbd75e344fe571c984cce654c8e855fee1ad88c7
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-20 19:31:17 -0500

    Add docs for RegionalRoute

--- Gwen/Type/RegionalRoute.cs
+++ Gwen/Type/RegionalRoute.cs
@@ -1,10 +1,17 @@
-﻿namespace Gwen.Type
+﻿using Gwen.Core;
+
+namespace Gwen.Type
 {
-    public enum RegionalRoute
-    {
-        Americas,
-        Asia,
-        Europe,
-        SouthEastAsia
-    }
+	/// <summary>
+	/// <para>An enum that serves as a representation of regional routing values.</para>
+	/// <para>See usage with <see cref="RegionRouteMapper"/>.</para>
+	/// <see href="https://developer.riotgames.com/docs/lol#routing-values_regional-routing-values"/>
+	/// </summary>
+	public enum RegionalRoute
+	{
+		Americas,
+		Asia,
+		Europe,
+		SouthEastAsia
+	}
 }
