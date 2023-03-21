271f94bee97102e1aec7bfc136aa88768f130d8d
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-20 19:26:05 -0500

    Add docs to LeagueTier

--- Gwen/Type/LeagueTier.cs
+++ Gwen/Type/LeagueTier.cs
@@ -1,12 +1,16 @@
 ﻿namespace Gwen.Type
 {
-    public enum LeagueTier
-    {
-        Iron,
-        Bronze,
-        Silver,
-        Gold,
-        Platinum,
-        Diamond
-    }
+	/// <summary>
+	/// <para>An enum that represents League ranks.</para>
+	/// <see href="https://developer.riotgames.com/apis#league-v4"/>
+	/// </summary>
+	public enum LeagueTier
+	{
+		Iron,
+		Bronze,
+		Silver,
+		Gold,
+		Platinum,
+		Diamond
+	}
 }
