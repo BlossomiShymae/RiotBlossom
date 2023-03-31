47606a641de1305a0001b944c7f3e1d78f7aa728
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-03-31 14:27:09 -0500

    Add monad-like container for async method chaining

---
+++ BlossomiShymae.GwenTests/SewingKit/NeedleTests.cs
@@ -0,0 +1,25 @@
+﻿using BlossomiShymae.Gwen.Core.Wrapper;
+using BlossomiShymae.Gwen.Dto.CDragon.Champion;
+using BlossomiShymae.Gwen.SewingKit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BlossomiShymae.GwenTests.SewingKit
+{
+    [TestClass()]
+    public class NeedleTests
+    {
+        [TestMethod()]
+        public async Task Needle_WithSummoner_ShouldWork()
+        {
+            IGwenClient gwen = StubConfig.Gwen;
+
+            var data = await Needle<string>.From(StubConfig.SummonerName)
+                .Bind(x => gwen.Riot.Summoner.GetByNameAsync(StubConfig.SummonerPlatform, x))
+                .Bind(x => gwen.Riot.ChampionMastery.ListBySummonerIdAsync(StubConfig.SummonerPlatform, x.Id))
+                .Bind(x => gwen.CDragon.GetChampionByIdAsync((int)x.First().ChampionId))
+                .WorkAsync();
+
+            Assert.IsInstanceOfType(data, typeof(Champion));
+        }
+    }
+}
