c2feacd691e7537509f2b93f996c77f413394088
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-04-07 02:34:22 -0500

    Append to previous commit

--- BlossomiShymae.RiotBlossom/Dto/DDragon/Champion/Image.cs
+++ BlossomiShymae.RiotBlossom/Dto/DDragon/Champion/Image.cs
@@ -1,5 +1,8 @@
 ﻿namespace BlossomiShymae.RiotBlossom.Dto.DDragon.Champion
 {
+    /// <summary>
+    /// UNDOCUMENTED
+    /// </summary>
     public record Image
     {
         public string Full { get; init; } = default!;
