d5861d3caa2eecd1e033cd608b07bbedf4cb1368
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-04-07 05:17:16 -0500

    Add comments

--- BlossomiShymae.RiotBlossom/Middleware/LimiterShaper.cs
+++ BlossomiShymae.RiotBlossom/Middleware/LimiterShaper.cs
@@ -1,5 +1,8 @@
 ﻿namespace BlossomiShymae.RiotBlossom.Middleware
 {
+    /// <summary>
+    /// An enum that represents the shaping mechanism for <see cref="AlgorithmicLimiter"/>.
+    /// </summary>
     public enum LimiterShaper
     {
         Burst,
