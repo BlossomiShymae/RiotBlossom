c7a674085a83ce1af6aa2970a8f7b26820e208d7
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-04-07 05:52:09 -0500

    Add comments

--- BlossomiShymae.RiotBlossom/Middleware/ExecuteInfo.cs
+++ BlossomiShymae.RiotBlossom/Middleware/ExecuteInfo.cs
@@ -1,5 +1,8 @@
 ﻿namespace BlossomiShymae.RiotBlossom.Middleware
 {
+    /// <summary>
+    /// The executing information of a HTTP message in the request/response middleware cycle.
+    /// </summary>
     public record ExecuteInfo
     {
         public string RoutingValue { get; init; } = string.Empty;
