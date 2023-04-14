30643ce6a26609941ecf9e45db6e7f3d5d5988d8
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-04-14 12:30:24 -0500

    Refactor tests

--- BlossomiShymae.RiotBlossomTests/Api/Riot/RiotApiTests.cs
+++ BlossomiShymae.RiotBlossomTests/Api/Riot/RiotApiTests.cs
@@ -2,7 +2,6 @@
 using BlossomiShymae.RiotBlossom.Dto.Riot.Summoner;
 using BlossomiShymae.RiotBlossom.Exception;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Collections.Immutable;
 
 namespace BlossomiShymae.RiotBlossomTests.Api.Riot
 {
@@ -19,59 +18,6 @@ namespace BlossomiShymae.RiotBlossomTests.Api.Riot
             });
 
             SummonerDto dto = await client.Riot.Summoner.GetByNameAsync(StubConfig.SummonerPlatform, StubConfig.SummonerName);
-        }
-
-        [TestMethod()]
-        [ExpectedException(typeof(WarningLimiterException))]
-        public async Task Api_RunIterativelyForLimiter_ShouldThrowWarningException()
-        {
-            IRiotBlossomClient client = StubConfig.ThrowingClient;
-
-            SummonerDto summonerDto = await client.Riot.Summoner.GetByNameAsync(StubConfig.SummonerPlatform, StubConfig.SummonerName);
-            ImmutableList<string> matchIds = await client.Riot.Match.ListIdsByPuuidAsync(StubConfig.SummonerPlatform, summonerDto.Puuid);
-
-            for (int i = 0; i < 121; i++)
-            {
-                await client.Riot.Match.GetByIdAsync(StubConfig.SummonerPlatform, matchIds.First());
-            }
-        }
-
-        [TestMethod()]
-        public async Task Api_RunIterativelyForLimiter_ShouldRunWithoutException()
-        {
-            IRiotBlossomClient client = StubConfig.Client;
-
-            SummonerDto summonerDto = await client.Riot.Summoner.GetByNameAsync(StubConfig.SummonerPlatform, StubConfig.SummonerName);
-            ImmutableList<string> matchIds = await client.Riot.Match.ListIdsByPuuidAsync(StubConfig.SummonerRegion, summonerDto.Puuid, new() { Count = 100 });
-
-            foreach (string matchId in matchIds)
-            {
-                await client.Riot.Match.GetByIdAsync(StubConfig.SummonerRegion, matchId);
-            }
-            Assert.IsTrue(true);
-        }
-
-        [TestMethod()]
-        [ExpectedException(typeof(WarningLimiterException))]
-        public async Task Api_WhenAllForLimiter_ShouldThrowWarningException()
-        {
-            IRiotBlossomClient client = StubConfig.ThrowingClient;
-
-            SummonerDto summonerDto = await client.Riot.Summoner.GetByNameAsync(StubConfig.SummonerPlatform, StubConfig.SummonerName);
-            ImmutableList<string> matchIds = await client.Riot.Match.ListIdsByPuuidAsync(StubConfig.SummonerPlatform, summonerDto.Puuid);
-
-            await Task.WhenAll(Enumerable.Range(0, 121).Select(x => client.Riot.Match.GetByIdAsync(StubConfig.SummonerPlatform, matchIds.First())));
-        }
-
-        [TestMethod()]
-        public async Task Api_RunWhenAllForLimiter_ShouldRunWithoutException()
-        {
-            IRiotBlossomClient client = StubConfig.Client;
-
-            SummonerDto summonerDto = await client.Riot.Summoner.GetByNameAsync(StubConfig.SummonerPlatform, StubConfig.SummonerName);
-            ImmutableList<string> matchIds = await client.Riot.Match.ListIdsByPuuidAsync(StubConfig.SummonerRegion, summonerDto.Puuid, new() { Count = 100 });
-
-            await Task.WhenAll(matchIds.Select(x => client.Riot.Match.GetByIdAsync(StubConfig.SummonerRegion, x)));
         }
     }
 }
---
+++ BlossomiShymae.RiotBlossomTests/LimiterTests.cs
@@ -0,0 +1,65 @@
+﻿using BlossomiShymae.RiotBlossom.Core;
+using BlossomiShymae.RiotBlossom.Dto.Riot.Summoner;
+using BlossomiShymae.RiotBlossom.Exception;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Immutable;
+
+namespace BlossomiShymae.RiotBlossomTests
+{
+    [TestClass()]
+    public class LimiterTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(WarningLimiterException))]
+        public async Task Api_RunIterativelyForLimiter_ShouldThrowWarningException()
+        {
+            IRiotBlossomClient client = StubConfig.ThrowingClient;
+
+            SummonerDto summonerDto = await client.Riot.Summoner.GetByNameAsync(StubConfig.SummonerPlatform, StubConfig.SummonerName);
+            ImmutableList<string> matchIds = await client.Riot.Match.ListIdsByPuuidAsync(StubConfig.SummonerPlatform, summonerDto.Puuid);
+
+            for (int i = 0; i < 121; i++)
+            {
+                await client.Riot.Match.GetByIdAsync(StubConfig.SummonerPlatform, matchIds.First());
+            }
+        }
+
+        [TestMethod()]
+        public async Task Api_RunIterativelyForLimiter_ShouldRunWithoutException()
+        {
+            IRiotBlossomClient client = StubConfig.Client;
+
+            SummonerDto summonerDto = await client.Riot.Summoner.GetByNameAsync(StubConfig.SummonerPlatform, StubConfig.SummonerName);
+            ImmutableList<string> matchIds = await client.Riot.Match.ListIdsByPuuidAsync(StubConfig.SummonerRegion, summonerDto.Puuid, new() { Count = 100 });
+
+            foreach (string matchId in matchIds)
+            {
+                await client.Riot.Match.GetByIdAsync(StubConfig.SummonerRegion, matchId);
+            }
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(WarningLimiterException))]
+        public async Task Api_WhenAllForLimiter_ShouldThrowWarningException()
+        {
+            IRiotBlossomClient client = StubConfig.ThrowingClient;
+
+            SummonerDto summonerDto = await client.Riot.Summoner.GetByNameAsync(StubConfig.SummonerPlatform, StubConfig.SummonerName);
+            ImmutableList<string> matchIds = await client.Riot.Match.ListIdsByPuuidAsync(StubConfig.SummonerPlatform, summonerDto.Puuid);
+
+            await Task.WhenAll(Enumerable.Range(0, 121).Select(x => client.Riot.Match.GetByIdAsync(StubConfig.SummonerPlatform, matchIds.First())));
+        }
+
+        [TestMethod()]
+        public async Task Api_RunWhenAllForLimiter_ShouldRunWithoutException()
+        {
+            IRiotBlossomClient client = StubConfig.Client;
+
+            SummonerDto summonerDto = await client.Riot.Summoner.GetByNameAsync(StubConfig.SummonerPlatform, StubConfig.SummonerName);
+            ImmutableList<string> matchIds = await client.Riot.Match.ListIdsByPuuidAsync(StubConfig.SummonerRegion, summonerDto.Puuid, new() { Count = 100 });
+
+            await Task.WhenAll(matchIds.Select(x => client.Riot.Match.GetByIdAsync(StubConfig.SummonerRegion, x)));
+        }
+    }
+}
