75b246b0f1f90f75f56f476573eac61b396f2fbe
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-04-16 07:02:41 -0500

    Add MatchDto

---
+++ BlossomiShymae.RiotBlossom/Dto/Riot/TftMatch/MatchDto.cs
@@ -0,0 +1,14 @@
+﻿namespace BlossomiShymae.RiotBlossom.Dto.Riot.TftMatch
+{
+    public record MatchDto
+    {
+        /// <summary>
+        /// The match metadata.
+        /// </summary>
+        public MetadataDto Metadata { get; init; } = new();
+        /// <summary>
+        /// The match info.
+        /// </summary>
+        public InfoDto Info { get; init; } = new();
+    }
+}
