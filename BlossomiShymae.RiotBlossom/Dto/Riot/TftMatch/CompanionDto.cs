dcf004da34ca9489baf5c8053fdcf549472b8bab
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-04-16 06:47:27 -0500

    Add CompanionDto

---
+++ BlossomiShymae.RiotBlossom/Dto/Riot/TftMatch/CompanionDto.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BlossomiShymae.RiotBlossom.Dto.Riot.TftMatch
+{
+    /// <summary>
+    /// UNDOCUMENTED
+    /// </summary>
+    public record CompanionDto
+    {
+        [JsonPropertyName("skin_id")]
+        public int SkinId { get; init; }
+        [JsonPropertyName("content_id")]
+        public string ContentId { get; init; } = default!;
+        public string Species { get; init; } = default!;
+    }
+}
