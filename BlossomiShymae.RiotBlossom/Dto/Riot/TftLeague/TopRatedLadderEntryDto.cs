ce8c98b623940e968c5c61935e2d05b6f6b68cf3
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-04-16 07:48:27 -0500

    Add TopRatedLadderEntryDto

---
+++ BlossomiShymae.RiotBlossom/Dto/Riot/TftLeague/TopRatedLadderEntryDto.cs
@@ -0,0 +1,27 @@
+﻿namespace BlossomiShymae.RiotBlossom.Dto.Riot.TftLeague
+{
+    public record TopRatedLadderEntryDto
+    {
+        /// <summary>
+        /// The encrypted summoner ID.
+        /// </summary>
+        public string SummonerId { get; init; } = default!;
+        /// <summary>
+        /// The summoner name.
+        /// </summary>
+        public string SummonerName { get; init; } = default!;
+        /// <summary>
+        /// (Legal values: ORANGE, PURPLE, BLUE, GREEN, GRAY)
+        /// </summary>
+        public string RatedTier { get; init; } = default!;
+        /// <summary>
+        /// The rated rating.
+        /// </summary>
+        public string RatedRating { get; init; } = default!;
+        /// <summary>
+        /// The amount of wins.
+        /// </summary>
+        public int Wins { get; init; }
+        public int PreviousUpdateLadderPosition { get; init; }
+    }
+}
