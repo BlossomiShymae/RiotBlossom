13e176194ae16538f5a2be44f7ef73559cdf046b
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-04-26 17:52:25 -0500

    Add PlayerDto

---
+++ BlossomiShymae.RiotBlossom/Dto/Riot/LorRanked/PlayerDto.cs
@@ -0,0 +1,18 @@
+﻿namespace BlossomiShymae.RiotBlossom.Dto.Riot.LorRanked
+{
+    public record PlayerDto
+    {
+        /// <summary>
+        /// The player name.
+        /// </summary>
+        public string Name { get; init; } = default!;
+        /// <summary>
+        /// The rank of player.
+        /// </summary>
+        public int Rank { get; init; }
+        /// <summary>
+        /// The League points of player.
+        /// </summary>
+        public int Lp { get; init; }
+    }
+}
