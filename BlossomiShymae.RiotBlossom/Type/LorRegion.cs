dd12663cdfb83cea7d0ac4a6d72cd59bad102c9c
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-04-26 18:38:24 -0500

    Add enum for LoR regions

---
+++ BlossomiShymae.RiotBlossom/Type/LorRegion.cs
@@ -0,0 +1,12 @@
+﻿namespace BlossomiShymae.RiotBlossom.Type
+{
+    /// <summary>
+    /// <para>An enum that serves as a representation of regional routing values for Legends of Runeterra.</para>
+    /// </summary>
+    public enum LorRegion
+    {
+        Americas,
+        Europe,
+        SouthEastAsia
+    }
+}
