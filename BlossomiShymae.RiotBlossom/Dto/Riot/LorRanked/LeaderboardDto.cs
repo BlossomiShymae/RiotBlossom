f1f10da92444990417959bb928eab860c84bb5d7
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-04-26 17:53:20 -0500

    Add LeaderboardDto

---
+++ BlossomiShymae.RiotBlossom/Dto/Riot/LorRanked/LeaderboardDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Immutable;
+
+namespace BlossomiShymae.RiotBlossom.Dto.Riot.LorRanked
+{
+    public record LeaderboardDto
+    {
+        /// <summary>
+        /// The list of players in Master tier.
+        /// </summary>
+        public ImmutableList<PlayerDto> Players { get; init; } = ImmutableList<PlayerDto>.Empty;
+    }
+}
