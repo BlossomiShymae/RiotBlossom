3702f2eba9eb44541662b6564788b9c1e8745a42
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-04-26 17:58:12 -0500

    Add PlayerDto for LorMatch

---
+++ BlossomiShymae.RiotBlossom/Dto/Riot/LorMatch/PlayerDto.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Immutable;
+using System.Text.Json.Serialization;
+
+namespace BlossomiShymae.RiotBlossom.Dto.Riot.LorMatch
+{
+    public record PlayerDto
+    {
+        /// <summary>
+        /// The player UUID.
+        /// </summary>
+        public string Puuid { get; init; } = default!;
+        /// <summary>
+        /// The ID for the deck played.
+        /// </summary>
+        [JsonPropertyName("deck_id")]
+        public string DeckId { get; init; } = default!;
+        /// <summary>
+        /// The code for the deck played.
+        /// </summary>
+        [JsonPropertyName("deck_code")]
+        public string DeckCode { get; init; } = default!;
+        public ImmutableList<string> Factions { get; init; } = ImmutableList<string>.Empty;
+        [JsonPropertyName("game_outcome")]
+        public string GameOutcome { get; init; } = default!;
+        /// <summary>
+        /// The order in which the players took turns.
+        /// </summary>
+        [JsonPropertyName("order_of_play")]
+        public int OrderOfPlayer { get; init; }
+    }
+}
