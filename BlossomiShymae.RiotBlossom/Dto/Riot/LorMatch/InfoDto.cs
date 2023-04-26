b188fc69d1915f5a18b31b424bb42bcc396db2ea
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-04-26 18:03:00 -0500

    Add InfoDto

---
+++ BlossomiShymae.RiotBlossom/Dto/Riot/LorMatch/InfoDto.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Immutable;
+using System.Text.Json.Serialization;
+
+namespace BlossomiShymae.RiotBlossom.Dto.Riot.LorMatch
+{
+    public record InfoDto
+    {
+        /// <summary>
+        /// The current game mode. (Legal values: Constructed, Expeditions, Tutorial)
+        /// </summary>
+        [JsonPropertyName("game_mode")]
+        public string GameMode { get; init; } = default!;
+        /// <summary>
+        /// The current game type. (Legal values: Ranked, Normal, AI, Tutorial, VanillaTrial, Singleton, StandardGauntlet)
+        /// </summary>
+        [JsonPropertyName("game_type")]
+        public string GameType { get; init; } = default!;
+        /// <summary>
+        /// The time the game has started in Coordinated Universal Time.
+        /// </summary>
+        [JsonPropertyName("game_start_time_utc")]
+        public string GameStartTimeUtc { get; init; } = default!;
+        /// <summary>
+        /// The current game version.
+        /// </summary>
+        [JsonPropertyName("game_version")]
+        public string GameVersion { get; init; } = default!;
+        /// <summary>
+        /// The players participating in game.
+        /// </summary>
+        public ImmutableList<PlayerDto> Players { get; init; } = ImmutableList<PlayerDto>.Empty;
+        /// <summary>
+        /// The total turns taken by both players.
+        /// </summary>
+        [JsonPropertyName("total_turn_count")]
+        public int TotalTurnCount { get; init; }
+    }
+}
