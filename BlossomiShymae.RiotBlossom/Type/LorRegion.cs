777d7e195b84e37e8115d4a993efc34e675d593d
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-04-26 18:46:17 -0500

    Add mapper for LorRegion

--- BlossomiShymae.RiotBlossom/Type/LorRegion.cs
+++ BlossomiShymae.RiotBlossom/Type/LorRegion.cs
@@ -1,7 +1,10 @@
-﻿namespace BlossomiShymae.RiotBlossom.Type
+﻿using BlossomiShymae.RiotBlossom.Core;
+
+namespace BlossomiShymae.RiotBlossom.Type
 {
     /// <summary>
     /// <para>An enum that serves as a representation of regional routing values for Legends of Runeterra.</para>
+    /// <para>See <see cref="LorRegionMapper"/>.</para>
     /// </summary>
     public enum LorRegion
     {
