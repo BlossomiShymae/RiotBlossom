d67afc24abf94fda3aadb5732ed7c01cac24fc5e
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-04-26 22:03:12 -0500

    Add ActDto

---
+++ BlossomiShymae.RiotBlossom/Dto/Riot/ValContent/ActDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BlossomiShymae.RiotBlossom.Dto.Riot.ValContent
+{
+    public record ActDto
+    {
+        public string Name { get; init; } = default!;
+        public LocalizedNamesDto? LocalizedNames { get; init; }
+        public string Id { get; init; } = default!;
+        public bool IsActive { get; init; }
+    }
+}
