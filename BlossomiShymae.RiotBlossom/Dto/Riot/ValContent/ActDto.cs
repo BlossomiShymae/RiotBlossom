84a525a57d81dd1d0154672e47042cc266efc3b5
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-04-26 22:06:30 -0500

    Add ContentItemDto

--- BlossomiShymae.RiotBlossom/Dto/Riot/ValContent/ActDto.cs
+++ BlossomiShymae.RiotBlossom/Dto/Riot/ValContent/ActDto.cs
@@ -3,6 +3,9 @@
     public record ActDto
     {
         public string Name { get; init; } = default!;
+        /// <summary>
+        /// This is excluded from respone when a locale is set.
+        /// </summary>
         public LocalizedNamesDto? LocalizedNames { get; init; }
         public string Id { get; init; } = default!;
         public bool IsActive { get; init; }
