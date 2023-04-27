62e0a70812f7bb94dbaa40af1455474278b427c3
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-04-26 22:23:52 -0500

    Add FinishingDamageDto

---
+++ BlossomiShymae.RiotBlossom/Dto/Riot/ValMatch/FinishingDamageDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BlossomiShymae.RiotBlossom.Dto.Riot.ValMatch
+{
+    public record FinishingDamageDto
+    {
+        public string DamageType { get; init; } = default!;
+        public string DamageItem { get; init; } = default!;
+        public bool IsSecondaryFireMode { get; init; }
+    }
+}
