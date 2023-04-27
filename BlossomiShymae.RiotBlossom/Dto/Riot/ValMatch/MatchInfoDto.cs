0e2a4097304ec7019e3dd63f01898cd49b565e60
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-04-27 10:18:44 -0500

    Append to previous commit

--- BlossomiShymae.RiotBlossom/Dto/Riot/ValMatch/MatchInfoDto.cs
+++ BlossomiShymae.RiotBlossom/Dto/Riot/ValMatch/MatchInfoDto.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BlossomiShymae.RiotBlossom.Dto.Riot.ValMatch
+﻿namespace BlossomiShymae.RiotBlossom.Dto.Riot.ValMatch
 {
-    internal class MatchInfoDto
+    public record MatchInfoDto
     {
+        public string MatchId { get; init; } = default!;
+        public string MapId { get; init; } = default!;
+        public int GameLengthMillis { get; init; }
+        public long GameStartMillis { get; init; }
+        public string ProvisioningFlowId { get; init; } = default!;
+        public bool IsCompleted { get; init; }
+        public string CustomGameName { get; init; } = default!;
+        public string QueueId { get; init; } = default!;
+        public string GameMode { get; init; } = default!;
+        public bool IsRanked { get; init; }
+        public string SeasonId { get; init; } = default!;
     }
 }
