b20d49423cfb3669fec15582941d854a9da9881f
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-04-27 18:15:31 -0500

    Add PrettyPrinter helper with test

---
+++ BlossomiShymae.RiotBlossom/Core/PrettyPrinter.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+
+namespace BlossomiShymae.RiotBlossom.Core
+{
+    /// <summary>
+    /// A helper class used for prettifying strings.
+    /// </summary>
+    public static class PrettyPrinter
+    {
+        private static readonly JsonSerializerOptions s_options = new()
+        {
+            WriteIndented = true,
+            AllowTrailingCommas = true,
+            Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+        };
+
+        /// <summary>
+        /// <para>Get the pretty string of object, hehe!</para>
+        /// </summary>
+        /// <exception cref="NotSupportedException"></exception>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static string GetString(object obj)
+            => JsonSerializer.Serialize(obj, options: s_options);
+    }
+}
---
+++ BlossomiShymae.RiotBlossomTests/Core/PrettyPrinterTests.cs
@@ -0,0 +1,22 @@
+﻿using BlossomiShymae.RiotBlossom.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace BlossomiShymae.RiotBlossomTests.Core
+{
+    [TestClass()]
+    public class PrettyPrinterTests
+    {
+        [TestMethod()]
+        public async Task Printer_WithNestedObject_ShouldReturnPrettyString()
+        {
+            IRiotBlossomClient client = StubConfig.Client;
+
+            var itemDictionary = await client.CommunityDragon.GetItemDictionaryAsync();
+            string prettyItems = PrettyPrinter.GetString(itemDictionary);
+            Trace.WriteLine(prettyItems);
+
+            Assert.IsTrue(prettyItems.Length > 0);
+        }
+    }
+}
