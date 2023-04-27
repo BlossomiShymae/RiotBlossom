f002261cb41920a736b3a50c444bfdd33a9ca04e
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-04-27 11:14:56 -0500

    Add ValStatus API

--- BlossomiShymae.RiotBlossom/Api/RiotApi.cs
+++ BlossomiShymae.RiotBlossom/Api/RiotApi.cs
@@ -73,6 +73,10 @@ namespace BlossomiShymae.RiotBlossom.Api
         /// The API for Tft-Summoner-v1 endpoints.
         /// </summary>
         ITftSummonerApi TftSummoner { get; }
+        /// <summary>
+        /// The API for Val-Status-v1 endpoint.
+        /// </summary>
+        IValStatusApi ValStatus { get; }
     }
 
     internal class RiotApi : IRiotApi
@@ -94,6 +98,7 @@ namespace BlossomiShymae.RiotBlossom.Api
         private readonly TftMatchApi _tftMatchApi;
         private readonly TftStatusApi _tftStatusApi;
         private readonly TftSummonerApi _tftSummonerApi;
+        private readonly ValStatusApi _valStatusApi;
         public IAccountApi Account => _accountApi;
         public IChampionApi Champion => _championApi;
         public IChampionMasteryApi ChampionMastery => _championMasteryApi;
@@ -111,6 +116,7 @@ namespace BlossomiShymae.RiotBlossom.Api
         public ITftMatchApi TftMatch => _tftMatchApi;
         public ITftStatusApi TftStatus => _tftStatusApi;
         public ITftSummonerApi TftSummoner => _tftSummonerApi;
+        public IValStatusApi ValStatus => _valStatusApi;
 
         public RiotApi(RiotHttpClient riotHttpClient)
         {
@@ -131,6 +137,7 @@ namespace BlossomiShymae.RiotBlossom.Api
             _tftMatchApi = new(riotHttpClient);
             _tftStatusApi = new(riotHttpClient);
             _tftSummonerApi = new(riotHttpClient);
+            _valStatusApi = new(riotHttpClient);
         }
     }
 }
