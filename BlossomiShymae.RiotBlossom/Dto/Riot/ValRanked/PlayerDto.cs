9f47e25ce4334f0123b7bc0f7cc1ded80d31735d
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-04-26 22:16:25 -0500

    Add PlayerDto

---
+++ BlossomiShymae.RiotBlossom/Dto/Riot/ValRanked/PlayerDto.cs
@@ -0,0 +1,21 @@
+﻿namespace BlossomiShymae.RiotBlossom.Dto.Riot.ValRanked
+{
+    public record PlayerDto
+    {
+        /// <summary>
+        /// The player UUID. May be omitted if player is anonymized.
+        /// </summary>
+        public string? Puuid { get; init; }
+        /// <summary>
+        /// The game name of player. May be omitted if player is anonymized.
+        /// </summary>
+        public string? GameName { get; init; }
+        /// <summary>
+        /// The tag line of player. May be omitted if player is anonymized.
+        /// </summary>
+        public string? TagLine { get; init; }
+        public long LeaderboardRank { get; init; }
+        public long RankedRating { get; init; }
+        public long NumberOfWins { get; init; }
+    }
+}
