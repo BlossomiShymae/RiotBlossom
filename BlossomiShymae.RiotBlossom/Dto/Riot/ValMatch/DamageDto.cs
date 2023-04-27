413f965a1f28ca538eb857ce6a059354841a53c5
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-04-26 22:22:59 -0500

    Add DamageDto

---
+++ BlossomiShymae.RiotBlossom/Dto/Riot/ValMatch/DamageDto.cs
@@ -0,0 +1,14 @@
+﻿namespace BlossomiShymae.RiotBlossom.Dto.Riot.ValMatch
+{
+    public record DamageDto
+    {
+        /// <summary>
+        /// The PUUID of affected player.
+        /// </summary>
+        public string Receiver { get; init; } = default!;
+        public int Damage { get; init; }
+        public int Legshots { get; init; }
+        public int Bodyshots { get; init; }
+        public int Headshots { get; init; }
+    }
+}
