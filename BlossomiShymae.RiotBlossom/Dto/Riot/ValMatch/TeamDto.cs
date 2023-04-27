caa3a56943685a990d71137a2966b89be12b2441
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-04-27 10:08:35 -0500

    Add TeamDto

---
+++ BlossomiShymae.RiotBlossom/Dto/Riot/ValMatch/TeamDto.cs
@@ -0,0 +1,26 @@
+﻿namespace BlossomiShymae.RiotBlossom.Dto.Riot.ValMatch
+{
+    public record TeamDto
+    {
+        /// <summary>
+        /// The arbitrary string, being Red and Blue in bomb modes, the PUUID of the player in deathmatch.
+        /// </summary>
+        public string TeamId { get; init; } = default!;
+        /// <summary>
+        /// Whether the team has won the game.
+        /// </summary>
+        public bool Won { get; init; }
+        /// <summary>
+        /// The amount of rounds played.
+        /// </summary>
+        public int RoundsPlayed { get; init; }
+        /// <summary>
+        /// The amount of rounds the team won.
+        /// </summary>
+        public int RoundsWon { get; init; }
+        /// <summary>
+        /// The team points scored. Number of kills in deathmatch.
+        /// </summary>
+        public int NumPoints { get; init; }
+    }
+}
