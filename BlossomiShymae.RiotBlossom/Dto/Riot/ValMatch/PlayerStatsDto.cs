10a3f2281f1a0fccc2150dce018428d3d7499047
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-04-27 10:11:41 -0500

    Add PlayerStatsDto

---
+++ BlossomiShymae.RiotBlossom/Dto/Riot/ValMatch/PlayerStatsDto.cs
@@ -0,0 +1,13 @@
+﻿namespace BlossomiShymae.RiotBlossom.Dto.Riot.ValMatch
+{
+    public record PlayerStatsDto
+    {
+        public int Score { get; init; }
+        public int RoundsPlayed { get; init; }
+        public int Kills { get; init; }
+        public int Deaths { get; init; }
+        public int Assists { get; init; }
+        public int PlaytimeMillis { get; init; }
+        public AbilityCastsDto AbilityCasts { get; init; } = new();
+    }
+}
