c4b95f64fed5929a563b7491637415f3b0575265
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-04-27 10:47:26 -0500

    Add enum for VALORANT regions

---
+++ BlossomiShymae.RiotBlossom/Type/ValRegion.cs
@@ -0,0 +1,15 @@
+﻿namespace BlossomiShymae.RiotBlossom.Type
+{
+    /// <summary>
+    /// <para>An enum that serves as a representation of regional routing values for VALORANT.</para>
+    /// </summary>
+    public enum ValRegion
+    {
+        NorthAmerica,
+        AsiaPacific,
+        Brazil,
+        Korea,
+        Europe,
+        LatinAmerica
+    }
+}
