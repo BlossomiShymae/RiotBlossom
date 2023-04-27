ee65f7276371d52768502b76703217b56f0b3c26
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-04-27 10:18:32 -0500

    Add MatchInfoDto

---
+++ BlossomiShymae.RiotBlossom/Dto/Riot/ValMatch/MatchInfoDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlossomiShymae.RiotBlossom.Dto.Riot.ValMatch
+{
+    internal class MatchInfoDto
+    {
+    }
+}
