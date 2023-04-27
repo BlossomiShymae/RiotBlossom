b927390b41eddd55e8d23230f01fd9a752e64399
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-04-27 10:21:39 -0500

    Add MatchlistEntryDto

---
+++ BlossomiShymae.RiotBlossom/Dto/Riot/ValMatch/MatchlistEntryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BlossomiShymae.RiotBlossom.Dto.Riot.ValMatch
+{
+    public record MatchlistEntryDto
+    {
+        public string MatchId { get; init; } = default!;
+        public long GameStartTimeMillis { get; init; }
+        public string TeamId { get; init; } = default!;
+    }
+}
