429c50d7aced8ba7dd659331631c0b09b3fa6dee
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-04-27 10:10:21 -0500

    Add AbilityCastsDto

---
+++ BlossomiShymae.RiotBlossom/Dto/Riot/ValMatch/AbilityCastsDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BlossomiShymae.RiotBlossom.Dto.Riot.ValMatch
+{
+    public record AbilityCastsDto
+    {
+        public int GrenadeCasts { get; init; }
+        public int Ability1Casts { get; init; }
+        public int Ability2Casts { get; init; }
+        public int UltimateCasts { get; init; }
+    }
+}
