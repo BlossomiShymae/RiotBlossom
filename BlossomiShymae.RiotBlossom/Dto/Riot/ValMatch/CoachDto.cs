635253462643290b5cbc2d1540a010ce93c51402
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-04-27 10:09:23 -0500

    Add CoachDto

---
+++ BlossomiShymae.RiotBlossom/Dto/Riot/ValMatch/CoachDto.cs
@@ -0,0 +1,8 @@
+﻿namespace BlossomiShymae.RiotBlossom.Dto.Riot.ValMatch
+{
+    public record CoachDto
+    {
+        public string Puuid { get; init; } = default!;
+        public string TeamId { get; init; } = default!;
+    }
+}
