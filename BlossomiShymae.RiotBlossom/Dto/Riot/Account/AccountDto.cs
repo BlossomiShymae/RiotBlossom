cc1518bb40feeefe5d1d3dfdfd6a257d9f94896d
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-05-01 08:56:12 -0500

    Fix bug where AccountDto has incorrect property types

--- BlossomiShymae.RiotBlossom/Dto/Riot/Account/AccountDto.cs
+++ BlossomiShymae.RiotBlossom/Dto/Riot/Account/AccountDto.cs
@@ -9,13 +9,13 @@ namespace BlossomiShymae.RiotBlossom.Dto.Riot.Account
         /// </summary>
         public string Puuid { get; init; } = default!;
         /// <summary>
-        /// The Riot game name associated with account. May be excluded from response if account does not have it..
+        /// The Riot game name associated with account. May be excluded from response if account does not have it.
         /// </summary>
-        public string GameName { get; init; } = default!;
+        public string? GameName { get; init; }
         /// <summary>
         /// The Riot tag line associated with account. May be excluded from response if account does not have it.
         /// </summary>
-        public string TagLine { get; init; } = default!;
+        public string? TagLine { get; init; }
 
         public override string ToString()
         {
