0e2f7c3907cbd9c3030495ab7fc309c2720e4bc6
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-05-11 13:14:32 -0500

    Add Cost

---
+++ BlossomiShymae.RiotBlossom/Dto/MerakiAnalytics/Champion/Cost.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Immutable;
+
+namespace BlossomiShymae.RiotBlossom.Dto.MerakiAnalytics.Champion
+{
+    /// <summary>
+    /// UNDOCUMENTED
+    /// </summary>
+    public record Cost
+    {
+        public ImmutableList<Modifier> Modifiers { get; set; } = ImmutableList<Modifier>.Empty;
+    }
+}
