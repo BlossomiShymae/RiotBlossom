d5b11b8c194a8dec947c4eb0bcbfb5c8b45b0314
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-05-11 13:35:16 -0500

    Add Skin

---
+++ BlossomiShymae.RiotBlossom/Dto/MerakiAnalytics/Champion/Skin.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Immutable;
+
+namespace BlossomiShymae.RiotBlossom.Dto.MerakiAnalytics.Champion
+{
+    /// <summary>
+    /// UNDOCUMENTED
+    /// </summary>
+    public record Skin
+    {
+        public string? Name { get; set; }
+        public int Id { get; set; }
+        public bool IsBase { get; set; }
+        public string? Availability { get; set; }
+        public string? FormatName { get; set; }
+        public bool LootEligible { get; set; }
+        public string? Cost { get; set; }
+        public int Sale { get; set; }
+        public string? Distribution { get; set; }
+        public string? Rarity { get; set; }
+        public ImmutableList<Chroma> Chromas { get; set; } = ImmutableList<Chroma>.Empty;
+        public string? Lore { get; set; }
+        public float Release { get; set; }
+        public ImmutableList<string> Set { get; set; } = ImmutableList<string>.Empty;
+        public string? SplashPath { get; set; }
+        public string? UncenteredSplashPath { get; set; }
+        public string? TilePath { get; set; }
+        public string? LoadScreenPath { get; set; }
+        public string? LoadScreenVintagePath { get; set; }
+        public bool NewEffects { get; set; }
+        public bool NewAnimations { get; set; }
+        public bool NewRecall { get; set; }
+        public bool NewVoice { get; set; }
+        public bool NewQuotes { get; set; }
+        public ImmutableList<string> VoiceActor { get; set; } = ImmutableList<string>.Empty;
+        public ImmutableList<string> SplashArtist { get; set; } = ImmutableList<string>.Empty;
+    }
+}
