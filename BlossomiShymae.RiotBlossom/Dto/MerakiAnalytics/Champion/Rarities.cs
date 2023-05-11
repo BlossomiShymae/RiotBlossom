f7ef5c7aeee31ab4104d9b934ea7b49d8c2a3bc5
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-05-11 13:27:29 -0500

    Add Rarities

---
+++ BlossomiShymae.RiotBlossom/Dto/MerakiAnalytics/Champion/Rarities.cs
@@ -0,0 +1,11 @@
+﻿namespace BlossomiShymae.RiotBlossom.Dto.MerakiAnalytics.Champion
+{
+    /// <summary>
+    /// UNDOCUMENTED
+    /// </summary>
+    public record Rarities
+    {
+        public int Rarity { get; set; }
+        public string? Region { get; set; }
+    }
+}
