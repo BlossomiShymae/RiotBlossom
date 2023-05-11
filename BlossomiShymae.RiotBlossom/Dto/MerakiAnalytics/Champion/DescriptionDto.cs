322921d3b905e96bf1666eb2da04776039b8e991
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-05-11 13:26:24 -0500

    Add DescriptionDto

---
+++ BlossomiShymae.RiotBlossom/Dto/MerakiAnalytics/Champion/DescriptionDto.cs
@@ -0,0 +1,11 @@
+﻿namespace BlossomiShymae.RiotBlossom.Dto.MerakiAnalytics.Champion
+{
+    /// <summary>
+    /// UNDOCUMENTED
+    /// </summary>
+    public record DescriptionDto
+    {
+        public string? Description { get; set; }
+        public string? Region { get; set; }
+    }
+}
