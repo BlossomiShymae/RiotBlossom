e625408df7b3f0bb350c47fb041e3f528fcf4234
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-05-11 14:15:09 -0500

    Add Item

---
+++ BlossomiShymae.RiotBlossom/Dto/MerakiAnalytics/Item/Item.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Immutable;
+
+namespace BlossomiShymae.RiotBlossom.Dto.MerakiAnalytics.Item
+{
+    /// <summary>
+    /// UNDOCUMENTED
+    /// </summary>
+    public record Item
+    {
+        public string? Name { get; set; }
+        public int Id { get; set; }
+        public int Tier { get; set; }
+        public ImmutableList<string> Rank { get; set; } = ImmutableList<string>.Empty;
+        public ImmutableList<int> BuildsFrom { get; set; } = ImmutableList<int>.Empty;
+        public ImmutableList<int> BuildsInto { get; set; } = ImmutableList<int>.Empty;
+        public int SpecialRecipe { get; set; }
+        public bool NoEffects { get; set; }
+        public bool Removed { get; set; }
+        public string? RequiredChampion { get; set; }
+        public string? RequiredAlly { get; set; }
+        public string? Icon { get; set; }
+        public string? SimpleDescription { get; set; }
+        public ImmutableList<string> Nicknames { get; set; } = ImmutableList<string>.Empty;
+        public ImmutableList<Passive> Passives { get; set; } = ImmutableList<Passive>.Empty;
+        public ImmutableList<Active> Actives { get; set; } = ImmutableList<Active>.Empty;
+        public Stats Stats { get; set; } = new();
+        public Shop Shop { get; set; } = new();
+        public string? IconOverlay { get; set; }
+    }
+}
