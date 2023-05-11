975c8f6b2d00752060721c32d7db39754a134f8b
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-05-11 13:13:40 -0500

    Add Cooldown

---
+++ BlossomiShymae.RiotBlossom/Dto/MerakiAnalytics/Champion/Cooldown.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Immutable;
+
+namespace BlossomiShymae.RiotBlossom.Dto.MerakiAnalytics.Champion
+{
+    /// <summary>
+    /// UNDOCUMENTED
+    /// </summary>
+    public record Cooldown
+    {
+        public ImmutableList<Modifier> Modifiers { get; set; } = ImmutableList<Modifier>.Empty;
+        public bool AffectedByCdr { get; set; }
+    }
+}
