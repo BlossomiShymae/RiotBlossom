75fc1bd6d6cf9e0cc790c340a7d33793fdec46a4
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-05-11 13:12:44 -0500

    Add Modifier

---
+++ BlossomiShymae.RiotBlossom/Dto/MerakiAnalytics/Champion/Modifier.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Immutable;
+
+namespace BlossomiShymae.RiotBlossom.Dto.MerakiAnalytics.Champion
+{
+    /// <summary>
+    /// UNDOCUMENTED
+    /// </summary>
+    public record Modifier
+    {
+        public ImmutableList<double> Values { get; set; } = ImmutableList<double>.Empty;
+        public ImmutableList<string> Units { get; set; } = ImmutableList<string>.Empty;
+    }
+}
