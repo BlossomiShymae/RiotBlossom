6910f7e683b26a0ad4ad5beb1da62c14aeca36ef
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-05-11 13:16:54 -0500

    Add Effect

---
+++ BlossomiShymae.RiotBlossom/Dto/MerakiAnalytics/Champion/Effect.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Immutable;
+
+namespace BlossomiShymae.RiotBlossom.Dto.MerakiAnalytics.Champion
+{
+    /// <summary>
+    /// UNDOCUMENTED
+    /// </summary>
+    public record Effect
+    {
+        public string? Description { get; set; }
+        public ImmutableList<Leveling> Leveling { get; set; } = ImmutableList<Leveling>.Empty;
+    }
+}
