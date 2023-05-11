de5cf9a6035f7489fb9c9acd89c13192b944e9fa
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-05-11 13:11:04 -0500

    Add AttributeRatings

---
+++ BlossomiShymae.RiotBlossom/Dto/MerakiAnalytics/Champion/AttributeRatings.cs
@@ -0,0 +1,19 @@
+﻿namespace BlossomiShymae.RiotBlossom.Dto.MerakiAnalytics.Champion
+{
+    /// <summary>
+    /// UNDOCUMENTED
+    /// </summary>
+    public record AttributeRatings
+    {
+        public int Damage { get; set; }
+        public int Toughness { get; set; }
+        public int Control { get; set; }
+        public int Mobility { get; set; }
+        public int Utility { get; set; }
+        public int AbilityReliance { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+        public int Magic { get; set; }
+        public int Difficulty { get; set; }
+    }
+}
