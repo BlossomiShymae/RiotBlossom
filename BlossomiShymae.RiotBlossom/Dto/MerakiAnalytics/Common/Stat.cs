5657f5146115dabcf111675a2d11a2993271a1e6
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-05-11 12:30:38 -0500

    Add Stat

---
+++ BlossomiShymae.RiotBlossom/Dto/MerakiAnalytics/Common/Stat.cs
@@ -0,0 +1,15 @@
+﻿namespace BlossomiShymae.RiotBlossom.Dto.MerakiAnalytics.Common
+{
+    /// <summary>
+    /// UNDOCUMENTED
+    /// </summary>
+    public record Stat
+    {
+        public double Flat { get; set; }
+        public double Percent { get; set; }
+        public double PerLevel { get; set; }
+        public double PercentPerLevel { get; set; }
+        public double PercentBase { get; set; }
+        public double PercentBonus { get; set; }
+    }
+}
