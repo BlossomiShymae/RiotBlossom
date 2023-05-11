0c96efc793278ccb0eb22185b393bcba840d9165
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-05-11 13:29:39 -0500

    Add Chroma

---
+++ BlossomiShymae.RiotBlossom/Dto/MerakiAnalytics/Champion/Chroma.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Immutable;
+
+namespace BlossomiShymae.RiotBlossom.Dto.MerakiAnalytics.Champion
+{
+    /// <summary>
+    /// UNDOCUMENTED
+    /// </summary>
+    public record Chroma
+    {
+        public string? Name { get; set; }
+        public long Id { get; set; }
+        public string? ChromaPath { get; set; }
+        public ImmutableList<string> Colors { get; set; } = ImmutableList<string>.Empty;
+        public ImmutableList<DescriptionDto> Descriptions { get; set; } = ImmutableList<DescriptionDto>.Empty;
+        public ImmutableList<Rarities> Rarities { get; set; } = ImmutableList<Rarities>.Empty;
+    }
+}
