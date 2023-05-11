5465bdf12b40405a84429d4a5943912162f684ae
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-05-11 14:05:34 -0500

    Add Prices

---
+++ BlossomiShymae.RiotBlossom/Dto/MerakiAnalytics/Item/Prices.cs
@@ -0,0 +1,12 @@
+﻿namespace BlossomiShymae.RiotBlossom.Dto.MerakiAnalytics.Item
+{
+    /// <summary>
+    /// UNDOCUMENTED
+    /// </summary>
+    public record Prices
+    {
+        public int Total { get; set; }
+        public int Combined { get; set; }
+        public int Sell { get; set; }
+    }
+}
