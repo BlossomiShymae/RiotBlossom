e144b80d4c13d3992cd299728b43777531c5e143
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-05-11 14:08:45 -0500

    Add Shop

---
+++ BlossomiShymae.RiotBlossom/Dto/MerakiAnalytics/Item/Shop.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Immutable;
+
+namespace BlossomiShymae.RiotBlossom.Dto.MerakiAnalytics.Item
+{
+    /// <summary>
+    /// UNDOCUMENTED
+    /// </summary>
+    public record Shop
+    {
+        public Prices Prices { get; set; } = new();
+        public bool Purchasable { get; set; }
+        public ImmutableList<string> Tags { get; set; } = ImmutableList<string>.Empty;
+    }
+}
