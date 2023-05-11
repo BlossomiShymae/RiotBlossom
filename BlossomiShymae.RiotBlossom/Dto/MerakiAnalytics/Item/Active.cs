775b51c330c1995bbbe2eab42316dd051dbe61d3
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-05-11 14:11:33 -0500

    Add Active

---
+++ BlossomiShymae.RiotBlossom/Dto/MerakiAnalytics/Item/Active.cs
@@ -0,0 +1,14 @@
+﻿namespace BlossomiShymae.RiotBlossom.Dto.MerakiAnalytics.Item
+{
+    /// <summary>
+    /// UNDOCUMENTED
+    /// </summary>
+    public record Active
+    {
+        public bool Unique { get; set; }
+        public string? Name { get; set; }
+        public string? Effects { get; set; }
+        public int Range { get; set; }
+        public float Cooldown { get; set; }
+    }
+}
