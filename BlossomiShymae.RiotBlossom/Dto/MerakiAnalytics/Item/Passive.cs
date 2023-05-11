582ab6823f76c17bcc7936aae4ef1ed6e69c98fd
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-05-11 14:10:40 -0500

    Add Passive

---
+++ BlossomiShymae.RiotBlossom/Dto/MerakiAnalytics/Item/Passive.cs
@@ -0,0 +1,16 @@
+﻿namespace BlossomiShymae.RiotBlossom.Dto.MerakiAnalytics.Item
+{
+    /// <summary>
+    /// UNDOCUMENTED
+    /// </summary>
+    public record Passive
+    {
+        public bool Unique { get; set; }
+        public bool Mythic { get; set; }
+        public string? Name { get; set; }
+        public string? Effects { get; set; }
+        public int Range { get; set; }
+        public string? Cooldown { get; set; }
+        public Stats Stats { get; set; } = new();
+    }
+}
