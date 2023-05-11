066c2f9f0f1496c94fd5d6128b735e7f7d250405
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-05-11 13:23:54 -0500

    Add Price

---
+++ BlossomiShymae.RiotBlossom/Dto/MerakiAnalytics/Champion/Price.cs
@@ -0,0 +1,12 @@
+﻿namespace BlossomiShymae.RiotBlossom.Dto.MerakiAnalytics.Champion
+{
+    /// <summary>
+    /// UNDOCUMENTED
+    /// </summary>
+    public record Price
+    {
+        public int BlueEssence { get; set; }
+        public int Rp { get; set; }
+        public int SaleRp { get; set; }
+    }
+}
