f87a90b1fd100cb67aa58d24163626b54b114fce
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-05-13 00:59:04 -0500

    Add extension methods for LeagueDivision

---
+++ BlossomiShymae.RiotBlossom/Extensions/LeagueDivisionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using BlossomiShymae.RiotBlossom.Core;
+using BlossomiShymae.RiotBlossom.Type;
+
+namespace BlossomiShymae.RiotBlossom.Extensions
+{
+    public static class LeagueDivisionExtensions
+    {
+        /// <summary>
+        /// Get the associated raw value e.g. "I", "II", "III", "IV".
+        /// </summary>
+        /// <param name="leagueDivision"></param>
+        /// <returns></returns>
+        public static string GetValue(this LeagueDivision leagueDivision)
+        {
+            return LeagueDivisionMapper.GetValue(leagueDivision);
+        }
+    }
+}
