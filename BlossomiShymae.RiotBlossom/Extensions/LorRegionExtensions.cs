291f144a70c12dd96dbf0cd99409d29a8014fd5d
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-05-13 01:04:25 -0500

    Add extension methods for LorRegion

---
+++ BlossomiShymae.RiotBlossom/Extensions/LorRegionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using BlossomiShymae.RiotBlossom.Core;
+using BlossomiShymae.RiotBlossom.Type;
+
+namespace BlossomiShymae.RiotBlossom.Extensions
+{
+    public static class LorRegionExtensions
+    {
+        /// <summary>
+        /// Get the raw routing value e.g. "americas", "sea".
+        /// </summary>
+        /// <param name="lorRegion"></param>
+        /// <returns></returns>
+        public static string GetId(this LorRegion lorRegion)
+        {
+            return LorRegionMapper.GetId(lorRegion);
+        }
+    }
+}
