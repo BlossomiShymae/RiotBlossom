bfbbc13ec93f438e1ca237170575ed562d52d938
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-05-14 10:49:56 -0500

    Add abstract record with tests

---
+++ BlossomiShymae.RiotBlossom/Dto/DataObject.cs
@@ -0,0 +1,21 @@
+﻿using BlossomiShymae.RiotBlossom.Core;
+
+namespace BlossomiShymae.RiotBlossom.Dto
+{
+    public abstract record DataObject<T> where T : notnull
+    {
+        private T? _data;
+        protected T Data
+        {
+            get => _data ?? throw new InvalidOperationException($"{nameof(Data)} property must be initialized for {typeof(T)}!");
+            set => _data = value;
+        }
+
+        public DataObject() { }
+
+        public override string ToString()
+        {
+            return PrettyPrinter.GetString(Data);
+        }
+    }
+}
