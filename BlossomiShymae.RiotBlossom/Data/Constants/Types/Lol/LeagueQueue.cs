5119d561ab643b84b9caa90609f9d91f51c93252
BlossomiShymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-10-10 11:39:09 -0500

    Fix bug where value was wrong case

--- BlossomiShymae.RiotBlossom/Data/Constants/Types/Lol/LeagueQueue.cs
+++ BlossomiShymae.RiotBlossom/Data/Constants/Types/Lol/LeagueQueue.cs
@@ -8,7 +8,7 @@ namespace BlossomiShymae.RiotBlossom.Data.Constants.Types.Lol
     /// </summary>
     public sealed record LeagueQueue : ValueEnum<string>
     {
-        public static readonly LeagueQueue RankedSolo5x5 = new("RANKED_SOLO_5X5");
+        public static readonly LeagueQueue RankedSolo5x5 = new("RANKED_SOLO_5x5");
         public static readonly LeagueQueue RankedFlexSummonersRift = new("RANKED_FLEX_SR");
         public static readonly LeagueQueue RankedFlexTeamfightTactics = new("RANKED_FLEX_TT");
 
