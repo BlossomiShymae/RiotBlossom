7ba2ca586b6518925a6d08c60a9047b965825605
Blossomi Shymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-10-11 13:49:00 -0500

    Add cache factory

---
+++ BlossomiShymae.RiotBlossom/Core/Cache/CacheFactory.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlossomiShymae.RiotBlossom.Core.Cache
+{
+    public class CacheFactory
+    {
+        public static Cache Create(CacheProvider cacheProvider)
+        {
+            var configuration = new CacheTTLConfiguration();
+
+            return cacheProvider switch 
+            {
+                CacheProvider.Empty => new EmptyCache(configuration),
+                CacheProvider.Memory => new MemoryCache(configuration),
+                CacheProvider.FileSystem => new FileSystemCache(configuration),
+                _ => new EmptyCache(configuration)
+            };
+        }
+    }
+}
