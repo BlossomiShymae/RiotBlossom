216691e1b68768b18b0af29dd6204dd41a023179
Blossomi Shymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-10-11 19:09:13 -0500

    Add console example and page

---
+++ BlossomiShymae.Console/Program.cs
@@ -0,0 +1,9 @@
+﻿using BlossomiShymae.RiotBlossom.Client;
+using BlossomiShymae.RiotBlossom.Data.Constants.Shards;
+
+// RIOT_API_KEY environment variable is automatically used!
+var client = RiotBlossomClient.Create();
+
+var summoner = await client.SummonerV4.GetByNameAsync(LeagueShard.NA1, "uwuie time");
+
+Console.WriteLine(summoner);
