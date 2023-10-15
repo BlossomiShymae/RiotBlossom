c25c5c34a797aa901bb48bd75a5a282ea1b3218e
Blossomi Shymae <87099578+BlossomiShymae@users.noreply.github.com>
2023-10-14 21:15:06 -0500

    Change documentation structure for examples

--- BlossomiShymae.Console/Program.cs
+++ BlossomiShymae.Console/Program.cs
@@ -1,8 +1,23 @@
 ﻿using BlossomiShymae.RiotBlossom.Client;
+using BlossomiShymae.RiotBlossom.Core;
+using BlossomiShymae.RiotBlossom.Core.Cache;
+using BlossomiShymae.RiotBlossom.Core.Limiting;
 using BlossomiShymae.RiotBlossom.Data.Constants.Shards;
+using Microsoft.Extensions.Logging;
 
 // RIOT_API_KEY environment variable is automatically used!
-var client = RiotBlossomClient.Create();
+// var client = RiotBlossomClient.Create();
+var client = RiotBlossomClient.Create(new ApiConfiguration()
+{
+  Key = Environment.GetEnvironmentVariable("RIOT_API_KEY"),
+  Cache = CacheFactory.Create(CacheProvider.Memory),
+  Limiter = LimiterFactory.Create(LimiterProvider.Burst),
+  Http = new(),
+  Logger = LoggerFactory.Create(configure =>
+  {
+    configure.AddConsole();
+  }).CreateLogger("RiotBlossom")
+});
 
 var summoner = await client.SummonerV4.GetByNameAsync(LeagueShard.NA1, "uwuie time");
 
